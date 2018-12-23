9e05f284cf6da21b91351f10c1895b39bdd4bf5d
kasptom <kasptom@gmail.com>
2018-12-23 18:56:55 +0100

    Finish part 1 of the 7th day

--- src/main/java/Day07.java
+++ src/main/java/Day07.java
@@ -1,89 +1,106 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day07 implements IAocTask {
 
+    int maxTaskId = 0;
+
     @Override
     public String getFileName() {
-        return "input_07_small.txt";
+        return "input_07.txt";
     }
 
     @Override
     public void solvePartOne(List<String> lines) {
-        HashMap<String, Step> possibleSteps = createAllPossibleSteps();
+        int[][] graph = createGraph(lines);
+        printGraph(graph);
 
+        runKahn(graph);
+    }
+
+    private void runKahn(int[][] graph) {
+        List<Integer> processingOrder = new ArrayList<>();
+        HashSet<Integer> independent = new HashSet<>();
+
+        for (int row = 0; row < maxTaskId; row++) {
+            if (isIndependent(graph[row])) {
+                independent.add(row);
+            }
+        }
+
+        independent.forEach(taskId -> System.out.printf("%c", taskId + 65));
+
+        while (!independent.isEmpty()) {
+            int independentId = independent.stream().min(Integer::compareTo).get();
+            independent.remove(independentId);
+            processingOrder.add(independentId);
+
+            for (int row = 0; row <= maxTaskId; row++) {
+
+                graph[row][independentId] = 0;
+                if (isIndependent(graph[row]) && !processingOrder.contains(row)) {
+                    independent.add(row);
+                }
+            }
+//            printGraph(graph);
+        }
+
+        System.out.println();
+        processingOrder.forEach(independentId -> System.out.printf("%c", independentId + 'A'));
+    }
+
+    private boolean isIndependent(int[] array) {
+        return Arrays.stream(array).noneMatch(dep -> dep == 1);
+    }
+
+    private int[][] createGraph(List<String> lines) {
         // Step C must be finished before step A can begin.
         String STEP_PAIR_REGEX = "Step ([A-Z]) must be finished before step ([A-Z]) can begin";
         Pattern pattern = Pattern.compile(STEP_PAIR_REGEX);
 
-        createDependencyTree(lines, possibleSteps, pattern);
+        int[][] graph = new int[26][26];
 
-        printStepsInExecutionOrder(possibleSteps);
-    }
-
-    private void printStepsInExecutionOrder(HashMap<String, Step> possibleSteps) {
-        HashSet<Step> dependentSteps =  new HashSet<>();
-
-        for (Step step : possibleSteps.values()) {
-            dependentSteps.addAll(possibleSteps
-                    .values()
-                    .stream()
-                    .filter(st -> step.childStepNames.contains(st.name))
-                    .collect(Collectors.toList()));
-        }
-
-        for (Step step : possibleSteps.values()) {
-            if (!dependentSteps.contains(step)) {
-                System.out.printf("Independent step: %s\n", step.name);
-            }
-        }
-    }
-
-    private void createDependencyTree(List<String> lines, HashMap<String, Step> possitbleSteps, Pattern pattern) {
         for (String line : lines) {
+
             Matcher matcher = pattern.matcher(line);
             if (matcher.find()) {
                 String prevName = matcher.group(1);
                 String nextName = matcher.group(2);
 
-                Step prevStep = possitbleSteps.get(prevName);
-                Step nextStep = possitbleSteps.get(nextName);
+                int nextId = (int) nextName.charAt(0) - 'A';
+                int prevId = (int) prevName.charAt(0) - 'A';
 
-                prevStep.addChildStep(nextStep);
+                maxTaskId = maxTaskId < prevId ? prevId : maxTaskId;
+                maxTaskId = maxTaskId < nextId ? nextId : maxTaskId;
+
+                graph[nextId][prevId]++;
             }
+        }
+
+        return graph;
+    }
+
+    private void printGraph(int[][] graph) {
+        System.out.println(" A B C D E F G H I J K L M N O P Q R S T U W X Y Z");
+        for (int i = 0; i <= maxTaskId; i++) {
+            System.out.print((char) ('A' + i));
+            for (int j = 0; j <= maxTaskId; j++) {
+                if (graph[i][j] == 1) {
+                    System.out.print("Z ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
     }
 
     @Override
     public void solvePartTwo(List<String> lines) {
 
-    }
-
-    private HashMap<String, Step> createAllPossibleSteps() {
-        HashMap<String, Step> steps = new HashMap<>();
-
-        for (char i = 'A'; i <= 'Z'; i++) {
-            String stepName = "" + i;
-            steps.put(stepName, new Step(stepName));
-        }
-
-        return steps;
-    }
-
-    class Step {
-        String name;
-        HashSet<String> childStepNames = new HashSet<>();
-
-        public Step(String name) {
-            this.name = name;
-        }
-
-        void addChildStep(Step step) {
-            childStepNames.add(step.name);
-        }
     }
 }
