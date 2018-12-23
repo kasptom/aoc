27d9894b822f0eb8e1cd24835f234a8c2b0ec7e5
kasptom <kasptom@gmail.com>
2018-12-23 21:42:37 +0100

    Almost finish the 2nd part

--- src/main/java/Day07.java
+++ src/main/java/Day07.java
@@ -7,11 +7,25 @@ import java.util.regex.Pattern;
 
 public class Day07 implements IAocTask {
 
-    int maxTaskId = 0;
+    private static final int NUMBER_OF_ELVES = 2;
+    private int BASE_TIME_SECONDS = 0;
+    private int maxTaskId = 0;
+    private int[] taskTimeLeft = createTaskTimes();
+
+    List<Elf> elves = new ArrayList<>();
+    HashSet<Integer> currentlyProcessed = new HashSet<>();
+
+    private int[] createTaskTimes() {
+        int[] times = new int[26];
+        for (int i = 0; i < 26; i++) {
+            times[i] = i + 1 + BASE_TIME_SECONDS;
+        }
+        return times;
+    }
 
     @Override
     public String getFileName() {
-        return "input_07.txt";
+        return "input_07_small.txt";
     }
 
     @Override
@@ -24,6 +38,27 @@ public class Day07 implements IAocTask {
 
     private void runKahn(int[][] graph) {
         List<Integer> processingOrder = new ArrayList<>();
+        HashSet<Integer> independent = createIndependentTaskSet(graph);
+
+        while (!independent.isEmpty()) {
+            int independentId = getNextIndependentId(independent);
+            processingOrder.add(independentId);
+            updateGraph(graph, processingOrder, independent, independentId);
+//            printGraph(graph);
+        }
+
+        processingOrder.forEach(independentId -> System.out.printf("%c", independentId + 'A'));
+        System.out.println();
+    }
+
+    private Integer getNextIndependentId(HashSet<Integer> independent) {
+        Integer independentId;
+        independentId = independent.stream().min(Integer::compareTo).orElse(-1);
+        independent.remove(independentId);
+        return independentId;
+    }
+
+    private HashSet<Integer> createIndependentTaskSet(int[][] graph) {
         HashSet<Integer> independent = new HashSet<>();
 
         for (int row = 0; row < maxTaskId; row++) {
@@ -33,24 +68,9 @@ public class Day07 implements IAocTask {
         }
 
         independent.forEach(taskId -> System.out.printf("%c", taskId + 65));
-
-        while (!independent.isEmpty()) {
-            int independentId = independent.stream().min(Integer::compareTo).get();
-            independent.remove(independentId);
-            processingOrder.add(independentId);
-
-            for (int row = 0; row <= maxTaskId; row++) {
-
-                graph[row][independentId] = 0;
-                if (isIndependent(graph[row]) && !processingOrder.contains(row)) {
-                    independent.add(row);
-                }
-            }
-//            printGraph(graph);
-        }
-
         System.out.println();
-        processingOrder.forEach(independentId -> System.out.printf("%c", independentId + 'A'));
+
+        return independent;
     }
 
     private boolean isIndependent(int[] array) {
@@ -101,6 +121,103 @@ public class Day07 implements IAocTask {
 
     @Override
     public void solvePartTwo(List<String> lines) {
+        for (int i = 1; i <= NUMBER_OF_ELVES; i++) {
+            elves.add(new Elf(i));
+        }
 
+        int[][] graph = createGraph(lines);
+//        printGraph(graph);
+
+        runKahnWithElves(graph, elves);
+    }
+
+    private void runKahnWithElves(int[][] graph, List<Elf> elves) {
+        List<Integer> processingOrder = new ArrayList<>();
+        HashSet<Integer> independent = createIndependentTaskSet(graph);
+
+        int second = -1;
+        while (!independent.isEmpty() || elves.stream().anyMatch(elf -> !elf.isAvailable())) {
+            second++;
+            elves.forEach(Elf::onClockTick);
+
+            elves.stream()
+                    .filter(Elf::isAvailable)
+                    .forEach(elf -> {
+                        if (elf.currentTaskId != -1) {
+                            processingOrder.add(elf.currentTaskId);
+                            updateGraph(graph, processingOrder, independent, elf.finishTask());
+                        }
+
+                        int taskId = getNextIndependentId(independent);
+
+                        if (taskId != -1) {
+                            elf.assignTask(taskId);
+                        }
+                    });
+
+            printWorkStatus(elves, second);
+//            printGraph(graph);
+        }
+
+        System.out.println();
+        processingOrder.forEach(independentId -> System.out.printf("%c", independentId + 'A'));
+    }
+
+    private void printWorkStatus(List<Elf> elves, int second) {
+        System.out.printf("%3d ", second);
+        elves.forEach(elf -> {
+            String currentTask = elf.currentTaskId != -1 ? "" + (char) (elf.currentTaskId + 'A') : ".";
+            System.out.print(currentTask + "\t");
+        });
+        System.out.println();
+    }
+
+    private void updateGraph(int[][] graph, List<Integer> processingOrder, HashSet<Integer> independent, int independentId) {
+        for (int row = 0; row <= maxTaskId; row++) {
+            graph[row][independentId] = 0;
+            if (isIndependent(graph[row]) && !processingOrder.contains(row) && !currentlyProcessed.contains(row)) {
+                independent.add(row);
+            }
+        }
+    }
+
+    class Elf {
+        int elfId;
+        int timeLeft;
+        int currentTaskId = -1;
+
+        Elf(int elfId) {
+            this(elfId, 0);
+        }
+
+        Elf(int elfId, int timeLeft) {
+            this.elfId = elfId;
+            this.timeLeft = timeLeft;
+        }
+
+        boolean isAvailable() {
+            return timeLeft == 0;
+        }
+
+        void assignTask(int taskId) {
+            this.currentTaskId = taskId;
+            currentlyProcessed.add(taskId);
+            this.timeLeft += taskTimeLeft[taskId];
+        }
+
+        int finishTask() {
+            currentlyProcessed.remove(currentTaskId);
+            timeLeft = 0;
+            int currentTaskId = this.currentTaskId;
+            this.currentTaskId = -1;
+            return currentTaskId;
+        }
+
+        private void onClockTick() {
+            this.timeLeft--;
+            if (timeLeft < 0) {
+                timeLeft = 0;
+            }
+        }
     }
 }
