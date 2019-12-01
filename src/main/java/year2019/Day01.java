c1097df8e552aa4ea08a333ddca3e9336e6b204b
kasptom <kasptom@gmail.com>
2019-12-01 08:38:34 +0100

    Prepare aoc2019 package

---
+++ src/main/java/year2019/Day01.java
@@ -0,0 +1,22 @@
+package year2019;
+
+import aoc.IAocTask;
+
+import java.util.List;
+
+public class Day01 implements IAocTask {
+    @Override
+    public String getFileName() {
+        return null;
+    }
+
+    @Override
+    public void solvePartOne(List<String> lines) {
+
+    }
+
+    @Override
+    public void solvePartTwo(List<String> lines) {
+
+    }
+}
