4ed64e9a3c5fc8263b11846f87fffbfad70a6767
kasptom <kasptom@gmail.com>
2019-12-03 07:55:53 +0100

    Solve part two

--- src/main/java/year2019/Day03.java
+++ src/main/java/year2019/Day03.java
@@ -2,10 +2,7 @@ package year2019;
 
 import aoc.IAocTask;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day03 implements IAocTask {
@@ -26,15 +23,17 @@ public class Day03 implements IAocTask {
                 .filter(p -> pathB.stream().anyMatch(q -> q.compareTo(p) == 0))
                 .min((a, b) -> Integer.compare(Math.abs(a.x) + Math.abs(a.y), Math.abs(b.x) + Math.abs(b.y)))
                 .orElse(null);
+
         System.out.println(Math.abs(closest.x) + Math.abs(closest.y));
     }
 
     private List<Point> generatePath(String[] wire) {
         List<Point> path = new ArrayList<>();
-        path.add(new Point(0, 0));
+        path.add(new Point(0, 0, 0));
 
+        int totalSteps = 0;
         for (int i = 0; i < wire.length; i++) {
-            String direction = wire[i].substring(0,1);
+            String direction = wire[i].substring(0, 1);
             Integer steps = Integer.valueOf(wire[i].substring(1));
 
             int x = 0, y = 0;
@@ -53,8 +52,9 @@ public class Day03 implements IAocTask {
             }
 
             for (int j = 0; j < steps; j++) {
+                totalSteps++;
                 Point a = path.get(path.size() - 1);
-                path.add(new Point(a.x + x, a.y + y));
+                path.add(new Point(a.x + x, a.y + y, totalSteps));
             }
         }
         return path;
@@ -62,14 +62,38 @@ public class Day03 implements IAocTask {
 
     @Override
     public void solvePartTwo(List<String> lines) {
+        String[] wireA = lines.get(0).split(",");
+        String[] wireB = lines.get(1).split(",");
+        List<Point> pathA = generatePath(wireA);
+        List<Point> pathB = generatePath(wireB);
+
+        List<Point> intersections = pathA.stream().filter(p -> p.x != 0 || p.y != 0)
+                .filter(p -> pathB.stream().anyMatch(q -> q.compareTo(p) == 0))
+                .collect(Collectors.toList());
+
+        List<Point> intersectionsB = pathB.stream().filter(p -> intersections.stream().anyMatch(q -> q.compareTo(p) == 0)).collect(Collectors.toList());
+
+        HashMap<Point, Point> combinedIntersections = new HashMap<>();
+        intersections.forEach(point -> {
+            combinedIntersections.put(point, intersectionsB.stream().filter(p -> p.compareTo(point) == 0).min(Comparator.comparingInt(p -> p.step)).orElse(null));
+        });
+
+        Integer pathLength = intersections.stream()
+                .map(p -> p.step + combinedIntersections.get(p).step)
+                .min(Integer::compareTo)
+                .orElse(null);
+
+        System.out.println(pathLength);
     }
 
-    class Point implements Comparable<Point>{
+    class Point implements Comparable<Point> {
+        private final int step;
         Integer x, y;
 
-        public Point(int i, int i1) {
+        public Point(int i, int i1, int step) {
             x = i;
             y = i1;
+            this.step = step;
         }
 
         @Override
