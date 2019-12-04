62f5d5aaef86bf15945dcc113c13584bad0e61b0
kasptom <kasptom@gmail.com>
2019-12-04 23:01:25 +0100

    Solve part1 of day 4

--- src/main/java/year2019/Day04.java
+++ src/main/java/year2019/Day04.java
@@ -5,13 +5,19 @@ import aoc.IAocTask;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Day04 implements IAocTask {
+
+    private int[] lowerArr;
+    private int[] upperArr;
+    private int lower;
+    private int upper;
+
+
     @Override
     public String getFileName() {
-        return "aoc/year2019/input_04.txt";
+        return "aoc2019/input_04.txt";
     }
 
     @Override
@@ -23,13 +29,75 @@ public class Day04 implements IAocTask {
         Integer upper = range.get(1);
 
         int count = countNumbersMeetingCriteria(lower, upper);
+        System.out.println(count);
     }
 
-    private int countNumbersMeetingCriteria(Integer lower, Integer upper) {
+    private int countNumbersMeetingCriteria(int lower, int upper) {
         int digitsCount = 6;
         int currentDigitIdx = 0;
+        int count = 0;
+        int[] number = new int[digitsCount];
+        lowerArr = new int[digitsCount];
+        upperArr = new int[digitsCount];
+        this.lower = lower;
+        this.upper = upper;
 
-        return 0;
+        int i = digitsCount - 1;
+        while (i >= 0) {
+            this.lowerArr[i] = lower % 10;
+            this.upperArr[i] = upper % 10;
+            lower /= 10;
+            upper /= 10;
+            i--;
+        }
+
+        for (i = 1; i <= 9; i++) {
+            count += countNumbers(currentDigitIdx, i, digitsCount, number);
+        }
+        return count;
+    }
+
+    private int countNumbers(int currentDigitIdx, int digit, int digitsCount, int[] number) {
+        number[currentDigitIdx] = digit;
+        if (currentDigitIdx == digitsCount - 1) {
+            boolean meetsCriteria = isInRange(number) && hasDouble(number);
+            if (meetsCriteria) {
+                print(number);
+            }
+            return meetsCriteria ? 1 : 0;
+        }
+        int sum = 0;
+        for (int i = digit; i <= 9; i++) {
+            sum += countNumbers(currentDigitIdx + 1, i, digitsCount, number);
+        }
+        return sum;
+    }
+
+    private void print(int[] number) {
+        for (int value : number) {
+            System.out.format("%d", value);
+        }
+        System.out.println();
+    }
+
+    private boolean isInRange(int[] numberArr) {
+        int number = 0;
+        for (int value : numberArr) {
+            number *= 10;
+            number += value;
+        }
+
+        return this.lower <= number && this.upper >= number;
+    }
+
+    private boolean hasDouble(int[] number) {
+        for (int i = 1; i < number.length; i++) {
+            if (number[i - 1] == number[i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
