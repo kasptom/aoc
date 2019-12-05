7ba14fe70ce73a3a1d8cc94d066562d938de5401
kasptom <kasptom@gmail.com>
2019-12-05 19:45:35 +0100

    Day5 Fix the new version of Int code computer (swap I with O)

--- src/main/java/year2019/Day05.java
+++ src/main/java/year2019/Day05.java
@@ -8,7 +8,8 @@ import static year2019.utils.Aoc2019Utils.*;
 
 public class Day05 implements IAocTask {
 
-    private int input;
+    private int inputOutput;
+    private int prevInstructionIdx;
 
     @Override
     public String getFileName() {
@@ -25,9 +26,12 @@ public class Day05 implements IAocTask {
 
     private void runProgram(int[] parsedCode) {
         int i = 0;
-        input = 1;
-        for (; i < parsedCode.length; ) {
+        inputOutput = 1;
+//        int executionCounter = 0;
+        for (; i < parsedCode.length; /* && executionCounter < 1e9; */ ) {
+            prevInstructionIdx = i;
             i = runInstructions(parsedCode, i);
+//            executionCounter++;
         }
     }
 
@@ -35,6 +39,7 @@ public class Day05 implements IAocTask {
     private int runInstructions(int[] parsedCode, int i) {
         int[] instruction = getInstruction(parsedCode[i]);
         if (instruction[IDX_OPCODE_A] + 10 * instruction[IDX_OPCODE_B] == INSTR_STOP) {
+            System.out.printf("TESTS PASSED!!! OUTPUT: %d\n", inputOutput);
             return parsedCode.length;
         }
 
@@ -44,17 +49,22 @@ public class Day05 implements IAocTask {
         } else if (instruction[IDX_OPCODE_A] == INSTR_MUL) {
             parsedCode[parsedCode[i + 3]] = multiplyNumbers(i, parsedCode, instruction);
             i += 4;
-        } else if (instruction[IDX_OPCODE_A] == INSTR_INPUT) {
-            input = instruction[IDX_MODE1] == MODE_IMMEDIATE
+        } else if (instruction[IDX_OPCODE_A] == INSTR_OUTPUT) {
+            inputOutput = instruction[IDX_MODE1] == MODE_IMMEDIATE
                     ? parsedCode[i + 1]
                     : parsedCode[parsedCode[i + 1]];
+            if (inputOutput != 0) {
+                String errMsg = String.format("Program test failed err=%d, instruction, parsedCode[%d] = %d", inputOutput, prevInstructionIdx, parsedCode[prevInstructionIdx]);
+                throw new RuntimeException(errMsg);
+            }
+
             i += 2;
-        } else if (instruction[IDX_OPCODE_A] == INSTR_OUTPUT) {
+        } else if (instruction[IDX_OPCODE_A] == INSTR_INPUT) {
             if (instruction[IDX_MODE1] == MODE_IMMEDIATE) {
                 throw new RuntimeException("writing parameter in immediate mode");
                 //parsedCode[i + 1] = input;
             } else { // MODE_POSITION
-                parsedCode[parsedCode[i + 1]] = input;
+                parsedCode[parsedCode[i + 1]] = inputOutput;
             }
             i += 2;
         }
@@ -65,7 +75,7 @@ public class Day05 implements IAocTask {
         int a = instruction[IDX_MODE1] == MODE_IMMEDIATE
                 ? parsedCode[i + 1]
                 : parsedCode[parsedCode[i + 1]];
-        int b = instruction[IDX_MODE1] == MODE_IMMEDIATE
+        int b = instruction[IDX_MODE2] == MODE_IMMEDIATE
                 ? parsedCode[i + 2]
                 : parsedCode[parsedCode[i + 2]];
         return a + b;
@@ -98,6 +108,12 @@ public class Day05 implements IAocTask {
         instruction[IDX_MODE2] = code % 10;
         code /= 10;
         instruction[IDX_MODE3] = code % 10;
+
+        if (instruction[IDX_MODE3] == MODE_IMMEDIATE) {
+            String errMsg = String.format("Immediate mode on instruction parsedCode[%d]=%d", prevInstructionIdx, code);
+            throw new RuntimeException(errMsg);
+        }
+
         return instruction;
     }
 
