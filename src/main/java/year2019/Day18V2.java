5e08388b21666b45db8c44a37c64a0a43a67f9ad
kasptom <kasptom@gmail.com>
2019-12-26 12:23:26 +0100

    Day18 (v2) Print all paths and items collected on them

--- src/main/java/year2019/Day18V2.java
+++ src/main/java/year2019/Day18V2.java
@@ -5,6 +5,7 @@ import year2019.utils.Aoc2019Utils;
 import year2019.utils.Pair;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day18V2 implements IAocTask {
 
@@ -13,6 +14,7 @@ public class Day18V2 implements IAocTask {
     String[][] maze;
     HashSet<Pair<Integer>> vertices;
     HashMap<String, Pair<Integer>> keysAndGatesCoordinates;
+    HashMap<Pair<Integer>, String> coordinatesToKeysAndGates;
 
     @Override
     public String getFileName() {
@@ -22,8 +24,17 @@ public class Day18V2 implements IAocTask {
     @Override
     public void solvePartOne(List<String> lines) {
         loadMaze(lines);
-        List<Path> shortestPaths = findShortestPath("@");
-        List<Path> shortestPaths2 = findShortestPath("A");
+        Map<String, List<Path>> keyOrGateToPath = keysAndGatesCoordinates
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        keyValue -> findShortestPaths(keyValue.getKey())
+                ));
+        keyOrGateToPath.forEach((key, value) -> {
+            System.out.printf("%n---Paths from: %s---%n", key);
+            value.forEach(System.out::println);
+        });
     }
 
     @Override
@@ -37,6 +48,7 @@ public class Day18V2 implements IAocTask {
         maze = new String[height][width];
         vertices = new HashSet<>();
         keysAndGatesCoordinates = new HashMap<>();
+        coordinatesToKeysAndGates = new HashMap<>();
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -46,19 +58,27 @@ public class Day18V2 implements IAocTask {
                 }
                 if (maze[i][j].matches("[A-Z]|[a-z]|@")) {
                     keysAndGatesCoordinates.put(maze[i][j], new Pair<>(j, i));
+                    coordinatesToKeysAndGates.put(new Pair<>(j, i), maze[i][j]);
                 }
             }
         }
     }
 
-    private List<Path> findShortestPath(String from) {
+    /**
+     * Finds the shortest paths (Dijkstra) from the root to all other nodes in the grid based labyrinth.
+     * Collects the data about found keys
+     *
+     * @param root root of the paths
+     * @return paths
+     */
+    private List<Path> findShortestPaths(String root) {
         HashMap<Pair<Integer>, Integer> unvisitedCost = new HashMap<>();
         vertices.forEach(vertex -> unvisitedCost.put(vertex, Integer.MAX_VALUE)); // all with max cost
 
         HashMap<Pair<Integer>, Pair<Integer>> childToParent = new HashMap<>();
 
         int pathCost = 0;
-        Pair<Integer> currentPosition = keysAndGatesCoordinates.get(from);
+        Pair<Integer> currentPosition = keysAndGatesCoordinates.get(root);
         unvisitedCost.put(currentPosition, pathCost);
 
         while (!unvisitedCost.isEmpty() && currentPosition != null) {
@@ -77,9 +97,7 @@ public class Day18V2 implements IAocTask {
             currentPosition = getUnvisitedNodeWithLowestCost(unvisitedCost);
         }
 
-        List<Path> paths = new ArrayList<>();
-        // TODO rewrite from childToParent to paths, save the required keys
-        return paths;
+        return mapToPaths(root, childToParent);
     }
 
     private Pair<Integer> getUnvisitedNodeWithLowestCost(HashMap<Pair<Integer>, Integer> unvisitedCost) {
@@ -94,17 +112,57 @@ public class Day18V2 implements IAocTask {
                 .findFirst().orElse(null);
     }
 
+    /**
+     * Each path starts with the root and ends with one of the coordinates in {@link Day18V2#keysAndGatesCoordinates}
+     *
+     * @param root          name of the root node
+     * @param childToParent shortest paths from the root to all other available non-wall nodes
+     * @return list of paths that end with key or gate (or @)
+     */
+    private List<Path> mapToPaths(String root, HashMap<Pair<Integer>, Pair<Integer>> childToParent) {
+        return keysAndGatesCoordinates
+                .keySet()
+                .stream()
+                .filter(keyOrGate -> !keyOrGate.equals(root))
+                .map(keyOrGate -> {
+                    Path path = new Path(root, keyOrGate);
+                    Pair<Integer> rootCoordinates = keysAndGatesCoordinates.get(root);
+                    Pair<Integer> currentCoordinates = keysAndGatesCoordinates.get(keyOrGate);
+                    while (!currentCoordinates.equals(rootCoordinates)) {
+                        path.path.add(currentCoordinates);
+                        if (coordinatesToKeysAndGates.containsKey(currentCoordinates)) {
+                            path.keysAndGatesOrdered.add(coordinatesToKeysAndGates.get(currentCoordinates));
+                        }
+                        currentCoordinates = childToParent.get(currentCoordinates);
+                    }
+                    Collections.reverse(path.path);
+                    Collections.reverse(path.keysAndGatesOrdered);
+                    return path;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
     static class Path {
         String from;
         String to;
         List<Pair<Integer>> path;
-        HashSet<String> requiredKeys;
+        List<String> keysAndGatesOrdered;
 
         public Path(String from, String to) {
             this.from = from;
             this.to = to;
             path = new ArrayList<>();
-            requiredKeys = new HashSet<>();
+            keysAndGatesOrdered = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            return "Path{" +
+                    "from='" + from + '\'' +
+                    ", to='" + to + '\'' +
+                    ", path=" + path +
+                    ", keysAndGatesOrdered=" + keysAndGatesOrdered +
+                    '}';
         }
     }
 }
