0a601d01d3d5be0b34bfdbf38308ad7c5012bb8e
kasptom <kasptom@gmail.com>
2020-12-01 21:01:09 +0100

    (Day01) Solve the 2nd part

--- src/main/java/year2020/Day01.java
+++ src/main/java/year2020/Day01.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day01 implements IAocTask {
+    private int SUM_TO_FIND = 2020;
+
     @Override
     public String getFileName() {
         return "aoc2020/input_01.txt";
@@ -14,13 +16,11 @@ public class Day01 implements IAocTask {
 
     @Override
     public void solvePartOne(List<String> lines) {
-        HashSet<Integer> complements = new HashSet<>();
-        List<Integer> expenses = lines.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        var complements = new HashSet<Integer>();
+        var expenses = getExpenses(lines);
         for (var expense : expenses) {
             if (complements.contains(expense)) {
-                System.out.println(expense * (2020 - expense));
+                System.out.println(expense * (SUM_TO_FIND - expense));
                 break;
             }
             complements.add(2020 - expense);
@@ -29,6 +29,25 @@ public class Day01 implements IAocTask {
 
     @Override
     public void solvePartTwo(List<String> lines) {
+        var expenses = getExpenses(lines);
+        for (int i = 0; i < expenses.size(); i++) {
+            for (int j = i + 1; j < expenses.size(); j++) {
+                for (int k = j + 1; k < expenses.size(); k++) {
+                    var a = expenses.get(i);
+                    var b = expenses.get(j);
+                    var c = expenses.get(k);
+                    if (a + b + c == SUM_TO_FIND) {
+                        System.out.println(a * b * c);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    private List<Integer> getExpenses(List<String> lines) {
+        return lines.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
