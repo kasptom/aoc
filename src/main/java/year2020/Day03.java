ba3c59c4d26613d53807cd38779684e6778b69ae
kasptom <kasptom@gmail.com>
2020-12-05 00:32:40 +0100

    (Day03) Solve the 2nd part

--- src/main/java/year2020/Day03.java
+++ src/main/java/year2020/Day03.java
@@ -3,6 +3,7 @@ package year2020;
 import aoc.IAocTask;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day03 implements IAocTask {
     private static final int MOVE_X = 3;
@@ -17,30 +18,19 @@ public class Day03 implements IAocTask {
     @Override
     public void solvePartOne(List<String> lines) {
         char[][] area = loadArea(lines);
-        int count = countTrees(area);
+        int count = countTrees(area, MOVE_X, MOVE_Y);
         System.out.println(count);
-    }
-
-    private int countTrees(char[][] area) {
-        int rowIdx = 0;
-        int colIdx = 0;
-        int count = 0;
-        int patternWidth = area[0].length;
-
-        while (rowIdx < area.length) {
-            if (area[rowIdx][colIdx] == TREE) {
-                count++;
-            }
-            rowIdx += MOVE_Y;
-            colIdx = (colIdx + MOVE_X) % patternWidth;
-        }
-
-        return count;
     }
 
     @Override
     public void solvePartTwo(List<String> lines) {
-
+        char[][] area = loadArea(lines);
+        int[] slopeX = {1, 3, 5, 7, 1};
+        int[] slopeY = {1, 1, 1, 1, 2};
+        int product = IntStream.range(0, slopeX.length)
+                .map(idx -> countTrees(area, slopeX[idx], slopeY[idx]))
+                .reduce(1, (x, y) ->x * y);
+        System.out.println(product);
     }
 
     private char[][] loadArea(List<String> lines) {
@@ -54,5 +44,22 @@ public class Day03 implements IAocTask {
             }
         }
         return area;
+    }
+
+    private int countTrees(char[][] area, int moveX, int moveY) {
+        int rowIdx = 0;
+        int colIdx = 0;
+        int count = 0;
+        int patternWidth = area[0].length;
+
+        while (rowIdx < area.length) {
+            if (area[rowIdx][colIdx] == TREE) {
+                count++;
+            }
+            rowIdx += moveY;
+            colIdx = (colIdx + moveX) % patternWidth;
+        }
+
+        return count;
     }
 }
