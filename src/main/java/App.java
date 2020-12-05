ca8ce786d0c5997269ea0e607d2e3419002eefa8
kasptom <kasptom@gmail.com>
2020-12-05 08:39:18 +0100

    (Day05) Binary Boarding
    (Day05) Solve the 1st part
    (Day05) Solve the 2nd part

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import aoc.IAocTask;
-import year2020.Day04;
+import year2020.Day05;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        IAocTask task = new Day04();
+        IAocTask task = new Day05();
 
         ClassLoader classLoader = task.getClass().getClassLoader();
         File inputFile = new File(Objects.requireNonNull(classLoader.getResource(task.getFileName())).getFile());
