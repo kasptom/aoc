35b0735c6f14e345be325e52894d480d8b7c97cd
Tomasz Kasprzyk <kasptom@users.noreply.github.com>
2020-12-05 02:20:07 +0100

    Feature/day 04 (#6)
    * (Day04) Solve the 1st part
    
    * (Day04) Solve the 2nd part

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import aoc.IAocTask;
-import year2020.Day03;
+import year2020.Day04;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        IAocTask task = new Day03();
+        IAocTask task = new Day04();
 
         ClassLoader classLoader = task.getClass().getClassLoader();
         File inputFile = new File(Objects.requireNonNull(classLoader.getResource(task.getFileName())).getFile());
