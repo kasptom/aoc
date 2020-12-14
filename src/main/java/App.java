7b233052376b44fa89f8cc78099899cfd77b1a16
kasptom <kasptom@gmail.com>
2020-12-14 12:27:50 +0100

    (Day 14) Docking Data
          * Solve the 1st part
          * 2nd part wip (not correct result)
          * Fix the bug
          * Solve the 2nd part (for real)
          * Clean up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import aoc.IAocTask;
-import year2020.Day13;
+import year2020.Day14;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        IAocTask task = new Day13();
+        IAocTask task = new Day14();
 
         ClassLoader classLoader = task.getClass().getClassLoader();
         File inputFile = new File(Objects.requireNonNull(classLoader.getResource(task.getFileName())).getFile());
