973a669794a6564f5a9c3b07f20aaf2d3b9d38b5
kasptom <kasptom@gmail.com>
2020-12-18 08:09:02 +0100

    (Day 18) Operation Order
          * wip
          * print stack
          * almost done, wip: int -> long
          * Solve the 1st part
          * Solve the 2nd part
          * TODO (;)) make it less ugly

---
+++ src/test/java/year2020/Day18TaskTest.java
@@ -0,0 +1,95 @@
+package year2020;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Day18TaskTest {
+    @Test
+    public void solve1_1() {
+        String input = "1 + 2 * 3 + 4 * 5 + 6";
+        Task task = Task.init(input);
+
+        assertEquals(71L, task.solve());
+    }
+
+    @Test
+    public void solve1_2() {
+        String input = "1 + (2 * 3) + (4 * (5 + 6))";
+        Task task = Task.init(input);
+
+        assertEquals(51, task.solve());
+    }
+
+    @Test
+    public void solve1_3() {
+        String input = "2 * 3 + (4 * 5)";
+        Task task = Task.init(input);
+
+        assertEquals(26, task.solve());
+    }
+
+    @Test
+    public void solve1_4() {
+        String input = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+        Task task = Task.init(input);
+
+        assertEquals(437, task.solve());
+    }
+
+    @Test
+    public void solve1_5() {
+        String input = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+        Task task = Task.init(input);
+
+        assertEquals(12240, task.solve());
+    }
+
+    @Test
+    public void solve1_6() {
+        String input = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+        Task task = Task.init(input);
+
+        assertEquals(13632, task.solve());
+    }
+
+    @Test
+    public void solve2_1() {
+        String input = "1 + (2 * 3) + (4 * (5 + 6))";
+        Task task = Task.init(input);
+
+        assertEquals(51, task.solve2());
+    }
+
+    @Test
+    public void solve2_2() {
+        String input = "2 * 3 + (4 * 5)";
+        Task task = Task.init(input);
+
+        assertEquals(46, task.solve2());
+    }
+
+    @Test
+    public void solve2_3() {
+        String input = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+        Task task = Task.init(input);
+
+        assertEquals(1445, task.solve2());
+    }
+
+    @Test
+    public void solve2_4() {
+        String input = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+        Task task = Task.init(input);
+
+        assertEquals(669060, task.solve2());
+    }
+
+    @Test
+    public void solve2_5() {
+        String input = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+        Task task = Task.init(input);
+
+        assertEquals(23340, task.solve2());
+    }
+}
