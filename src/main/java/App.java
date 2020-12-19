fbb58fe8973024c444c7d840c4de67c30914bc2f
kasptom <kasptom@gmail.com>
2020-12-19 07:55:40 +0100

    (Day 19) Monster Messages
          * Parse the rules
          * Solve the test case
          * wip 1st part
          * Solve the 1st part
          * (part 1) simplify the model
          * get the regex
          * use the regex solve the 1st part
          * Build rules 8 and 11, solve the 2nd part
          * clean up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import aoc.IAocTask;
-import year2020.Day18;
+import year2020.Day19;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        IAocTask task = new Day18();
+        IAocTask task = new Day19();
 
         ClassLoader classLoader = task.getClass().getClassLoader();
         File inputFile = new File(Objects.requireNonNull(classLoader.getResource(task.getFileName())).getFile());
