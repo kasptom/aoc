84afac25e4accb9a41b787fe7f8fa5d6d291e271
kasptom <kasptom@gmail.com>
2020-12-22 07:09:21 +0100

    (Day 22) Crab Combat
          * Start the 1st part
          * Solve the 1st part
          * Test case works but the real not
          * wip, wrong answer
          * wip so error on the 2nd part input
          * fix: separate snapshots
          * clean up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import aoc.IAocTask;
-import year2020.Day21;
+import year2020.Day22;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
-        IAocTask task = new Day21();
+        IAocTask task = new Day22();
 
         ClassLoader classLoader = task.getClass().getClassLoader();
         File inputFile = new File(Objects.requireNonNull(classLoader.getResource(task.getFileName())).getFile());
