5fb67fcccce67cf61d26b4370e31504c9bea476a
kasptom <kasptom@gmail.com>
2020-12-25 22:02:45 +0100

    (stats) Allow to specify the input and output json

--- src/main/java/stats/StatsApp.java
+++ src/main/java/stats/StatsApp.java
@@ -13,15 +13,28 @@ import java.nio.file.Path;
 
 public class StatsApp {
     public static void main(String[] args) throws IOException {
-        byte[] jsonData = getData();
+        String jsonFilePath = "stats/private_leaderboard.json";
+        String outputFilePath = null;
+        if (args.length >= 2) {
+            outputFilePath = args[1];
+        }
+        if (args.length >= 1) {
+            jsonFilePath = args[0];
+        }
+        System.out.format("Using %s as the input%n", jsonFilePath);
+        System.out.format("Using %s for the output%n", outputFilePath == null
+                ? "the default name \"output_{event}_{ownerId}.html\""
+                : "\"" + outputFilePath + "\" name");
+
+        byte[] jsonData = getData(jsonFilePath);
         ObjectMapper objectMapper = getObjectMapper();
         StatsDto dto = objectMapper.readValue(jsonData, StatsDto.class);
         Stats stats = StatsFactory.create(dto);
-        StatsPrinter.print(stats);
+        StatsPrinter.print(stats, outputFilePath);
     }
 
-    private static byte[] getData() throws IOException {
-        Path fullPath = FileUtils.getResourcePath("stats/private_leaderboard.json");
+    private static byte[] getData(String jsonFilePath) throws IOException {
+        Path fullPath = FileUtils.getResourcePath(jsonFilePath);
         return Files.readAllBytes(fullPath);
     }
 
--- src/main/java/stats/StatsPrinter.java
+++ src/main/java/stats/StatsPrinter.java
@@ -12,12 +12,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 public class StatsPrinter {
-    static void print(Stats stats) throws IOException {
+    static void print(Stats stats, String outputFileName) throws IOException {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setPrefix("templates/");
         templateResolver.setSuffix(".html");
@@ -32,12 +33,19 @@ public class StatsPrinter {
         variables.put("days", stats.getDays());
         IContext context = new Context(Locale.getDefault(), variables);
 
+        if (outputFileName == null) {
 //        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd_HHmmss");
 //        String timestamp = LocalDateTime.now().format(df);
 //        PrintWriter out = new PrintWriter(new FileOutputStream("output_" + timestamp + ".html"));
-        PrintWriter out = new PrintWriter(new FileOutputStream("output.html"), true, StandardCharsets.UTF_8);
+            outputFileName = "output_"+ stats.getEvent() + "_" + stats.getOwnerId() + ".html";
+        }
+        outputFileName = "./" + outputFileName;
+
+        PrintWriter out = new PrintWriter(new FileOutputStream(outputFileName), true, StandardCharsets.UTF_8);
         Files.copy(FileUtils.getResourcePath("templates/stats.css"), new FileOutputStream("stats.css"));
         templateEngine.process("stats", context, out);
         out.flush();
+        System.out.format("Stats can be found in: %s alongside with the stats.css file%n",
+                Paths.get(outputFileName).getFileName().toAbsolutePath());
     }
 }
