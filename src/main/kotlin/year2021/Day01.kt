3f422d018e546e8c6b40bd713a2392e51fc2f8b9
kasptom <kasptom@gmail.com>
2021-12-01 07:09:27 +0100

    (Day 01) Sonar Sweep

--- src/main/kotlin/year2021/Day01.kt
+++ src/main/kotlin/year2021/Day01.kt
@@ -6,10 +6,23 @@ class Day01 : IAocTaskKt {
 
     override fun getFileName() = "aoc2021/input_01.txt"
     override fun solvePartOne(lines: List<String>) {
-        TODO("Not yet implemented")
+        val depths = lines.map { Integer.parseInt(it) }
+        var increases = 0
+        for (idx in 1 until depths.size) {
+            if (depths[idx - 1] < depths[idx]) increases++
+        }
+        println(increases)
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        TODO("Not yet implemented")
+        val depths = lines.map { Integer.parseInt(it) }
+        var increases = 0
+        var prevSum = depths[0] + depths[1] + depths[2]
+        for (idx in 1..depths.size - 3) {
+            val sum = depths[idx] + depths[idx + 1] + depths[idx + 2]
+            if (sum > prevSum) increases++
+            prevSum = sum
+        }
+        println(increases)
     }
 }
