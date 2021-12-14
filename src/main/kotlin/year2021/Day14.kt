23c9fb804b130bf267c1dfe8dfbbc3f9ec8e34ce
kasptom <kasptom@gmail.com>
2021-12-14 07:02:52 +0100

    (Day 14) 1st part

--- src/main/kotlin/year2021/Day14.kt
+++ src/main/kotlin/year2021/Day14.kt
@@ -3,45 +3,48 @@ package year2021
 import aoc.IAocTaskKt
 
 class Day14 : IAocTaskKt {
-    override fun getFileName(): String = "aoc2021/input_14_test.txt"
+    override fun getFileName(): String = "aoc2021/input_14.txt"
 
     override fun solvePartOne(lines: List<String>) {
-        val polymer = lines[0].windowed(2)
-            .groupingBy { it }
-            .eachCount()
-            .toMutableMap()
+        var polymer = lines[0].chunked(1)
 
         val rules = lines.subList(1, lines.size)
             .map(Rule::parse)
             .groupBy { it.from }
             .mapValues { it.value.first().to }
+            .toMutableMap()
+
+//        rules.clear()
 
         for (step in 1..10) {
-            val newPolymer = polymer.toMutableMap()
-            for (rule in rules) {
-                if (polymer.containsKey(rule.key)) {
-                    val parentCount = polymer[rule.key]!!
-                    val left = rule.key[0] + rules[rule.key]!!
-                    val right = rules[rule.key] + rule.key[1]
-                    newPolymer.putIfAbsent(left, 0)
-                    newPolymer.putIfAbsent(right, 0)
-                    newPolymer[left] = newPolymer[left]!! + parentCount
-                    newPolymer[right] = newPolymer[right]!! + parentCount
+
+//            val left = rule.key[0] + rules[rule.key]!!
+//            val right = rules[rule.key] + rule.key[1]
+            val newPolymer = mutableListOf<String>()
+
+            for (idx in 1 until polymer.size) {
+                val key = polymer[idx - 1] + polymer[idx]
+                if (rules.containsKey(key)) {
+                    newPolymer.add(polymer[idx - 1])
+                    newPolymer.add(rules[key]!!)
+                } else {
+                    newPolymer.add(polymer[idx])
                 }
             }
-            polymer.clear()
-            polymer.putAll(newPolymer)
+            newPolymer.add(polymer.last())
+
+            polymer = newPolymer
+//            println(newPolymer)
         }
-        val result = polymer.map { listOf(Pair(it.key[0], it.value), Pair(it.key[1], it.value)) }
-            .flatten()
-            .groupBy { it.first }
-            .mapValues { it.value.sumOf { charCount -> charCount.second } }
+        val result = polymer
+            .groupingBy { it }
+            .eachCount()
             .toMap()
 
         val minCount = result.values.minOf { it }
         val maxCount = result.values.maxOf { it }
 
-        println(polymer)
+//        println(polymer)
         println(rules)
         println(result)
         println("$maxCount - $minCount = ${maxCount - minCount}")
