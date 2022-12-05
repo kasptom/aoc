002c2bbe232bd2fbe3e9adb3ee2d5add9df03f06
kasptom <kasptom@gmail.com>
2022-12-05 07:04:57 +0100

    (Day 05) Supply Stacks (part 2)

--- src/main/kotlin/year2022/Day05.kt
+++ src/main/kotlin/year2022/Day05.kt
@@ -8,29 +8,9 @@ class Day05 : IAocTaskKt {
     override fun solvePartOne(lines: List<String>) {
         val stackIndicesLineIdx = lines.indexOfFirst { it.trim().startsWith("1") }
         val stackSize = stackIndicesLineIdx
-        val containers = lines[stackIndicesLineIdx].trim().split(" ").filter { it.isNotEmpty() }
-            .map { it.toInt() }
-            .map { Container(it) }
-
-        for (stackLevel in 0 until stackSize) {
-            val level = lines[stackLevel]
-                .replace("    ", "- ")
-                .split(" ")
-                .map {
-                    it.replace("[", "")
-                        .replace("]", "")
-                }
-
-            for (container in containers) {
-                if (level.size >= container.id && level[container.id - 1] != "-") {
-                    container.stack += level[container.id - 1]
-                }
-            }
-        }
-
+        val containers = containers(lines, stackIndicesLineIdx, stackSize)
         val movesStartIdx = stackSize + 2
-        val moves = lines.subList(movesStartIdx, lines.size)
-            .map { Move.parse(it) }
+        val moves = moves(lines, movesStartIdx)
 
 //        println(containers)
 //        println(moves)
@@ -47,8 +27,60 @@ class Day05 : IAocTaskKt {
             .let { println(it) }
     }
 
+    private fun moves(
+        lines: List<String>,
+        movesStartIdx: Int,
+    ) = lines.subList(movesStartIdx, lines.size)
+        .map { Move.parse(it) }
+
+    private fun containers(
+        lines: List<String>,
+        stackIndicesLineIdx: Int,
+        stackSize: Int,
+    ): List<Container> {
+        val containers = lines[stackIndicesLineIdx].trim().split(" ").filter { it.isNotEmpty() }
+            .map { it.toInt() }
+            .map { Container(it) }
+
+        for (stackLevel in 0 until stackSize) {
+            val level = lines[stackLevel]
+                .replace("    ", "- ")
+                .split(" ")
+                .map {
+                    it.replace("[", "")
+                        .replace("]", "")
+                        .replace("-", "")
+                }
+
+            for (container in containers) {
+                if (level.size >= container.id && level[container.id - 1] != "-" && level[container.id - 1] != "") {
+                    container.stack += level[container.id - 1]
+                }
+            }
+        }
+        return containers
+    }
+
     override fun solvePartTwo(lines: List<String>) {
-        println("??")
+        val stackIndicesLineIdx = lines.indexOfFirst { it.trim().startsWith("1") }
+        val stackSize = stackIndicesLineIdx
+        val containers = containers(lines, stackIndicesLineIdx, stackSize)
+        val movesStartIdx = stackSize + 2
+        val moves = moves(lines, movesStartIdx)
+
+        for (move in moves) {
+            val fromContainer = containers.first { it.id == move.fromId }
+            val toContainer = containers.first { it.id == move.toId }
+            val toAdd = mutableListOf<String>()
+            for (step in 1..move.count) {
+                val removed = fromContainer.stack.removeFirst()
+                toAdd.add(removed)
+            }
+            toContainer.stack.addAll(0, toAdd)
+        }
+
+        containers.joinToString("") { it.stack.firstOrNull() ?: "" }
+            .let { println(it) }
     }
 
     data class Container(val id: Int, val stack: MutableList<String> = mutableListOf())
