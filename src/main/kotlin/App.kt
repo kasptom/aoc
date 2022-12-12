cee86254428df07943ef8d23ec2ac47c0f715f7d
kasptom <kasptom@gmail.com>
2022-12-12 08:24:57 +0100

    (Day 12) Hill Climbing Algorithm (part 1)

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,9 @@
 import aoc.IAocTaskKt
 import utils.InputReader
-import year2022.Day11
+import year2022.Day12
 
 fun main() {
-    val task: IAocTaskKt = Day11()
+    val task: IAocTaskKt = Day12()
     val lines: List<String> = InputReader(task.getFileName())
         .readLines()
 
