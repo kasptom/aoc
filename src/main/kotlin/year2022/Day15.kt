e3399e2602106e15651f5e77da0e17107465463c
kasptom <kasptom@gmail.com>
2022-12-15 07:50:22 +0100

    (Day 15) Beacon Exclusion Zone (part 2)

--- src/main/kotlin/year2022/Day15.kt
+++ src/main/kotlin/year2022/Day15.kt
@@ -2,13 +2,16 @@ package year2022
 
 import aoc.IAocTaskKt
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class Day15 : IAocTaskKt {
     override fun getFileName(): String = "aoc2022/input_15.txt"
 
     override fun solvePartOne(lines: List<String>) {
         val sensors: List<Sensor> = lines.map { Sensor.parse(it) }
-        val result = countNotPossiblePositions(sensors, 2000000)
+        val y = if (getFileName().contains("test")) 20 else 2000000
+        val result = countNotPossiblePositions(sensors, y)
         println(result - 1)
     }
 
@@ -20,14 +23,42 @@ class Day15 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        println("??")
+        val sensors: List<Sensor> = lines.map { Sensor.parse(it) }
+        val maxDistance = if (getFileName().contains("test")) 20 else 4000000
+//        val distressBeaconPosition = findPositionInRange(sensors, 20)
+        val distressBeaconPosition = findPositionInRange(sensors, maxDistance)
+        println(distressBeaconPosition)
+        val freq: Long = distressBeaconPosition.getTuningFrequency()
+        println(freq)
+    }
+
+    private fun findPositionInRange(sensors: List<Sensor>, maxDistance: Int): Point {
+        for (y in 0..maxDistance) { // TODO
+//        for (y in maxDistance..maxDistance) {
+            val notOverlappingRanges: List<RowRange> =
+                sensors.map { it.toLimitedNotPossiblePositionsFor(y, maxDistance) }
+                    .filter { it != RowRange.empty() }
+                    .fold(emptyList()) { notOverlapping, next ->
+                        val overlappingWithNext = notOverlapping.filter { it.overlaps(next) }
+                        val merged: RowRange = (overlappingWithNext + next).toRange()
+                        val result = (notOverlapping - overlappingWithNext.toSet()) + listOf(merged)
+                        result
+                    }
+            if (notOverlappingRanges.count() != 1) {
+                val coordsSorted = notOverlappingRanges.flatMap { listOf(it.fromX, it.toX) }
+                    .sorted()
+                val x = coordsSorted[1] + 1
+                return Point(x, y)
+            }
+        }
+        return Point(0, 0)
     }
 
     // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
     data class Sensor(val position: Point, val closestBeacon: Point) {
         fun toNotPossiblePositionsFor(y: Int): List<Point> {
             val notPossible = mutableListOf<Point>()
-            if (closestBeacon.y == y) notPossible += closestBeacon
+//            if (closestBeacon.y == y) notPossible += closestBeacon
             val closestManhattan = position.manhattan(closestBeacon)
             val verticalDistance = abs(y - position.y)
             if (verticalDistance > closestManhattan) return emptyList()
@@ -40,6 +71,21 @@ class Day15 : IAocTaskKt {
             }
             return notPossible
         }
+
+        fun toLimitedNotPossiblePositionsFor(y: Int, maxX: Int): RowRange {
+            mutableListOf<Point>()
+            val closestManhattan = position.manhattan(closestBeacon)
+            val verticalDistance = abs(y - position.y)
+            if (verticalDistance > closestManhattan) return RowRange.empty()
+
+            val horizontalDistance = (closestManhattan - verticalDistance)
+            return RowRange(
+                max(0, position.x - (horizontalDistance)),
+                min(position.x + (horizontalDistance), maxX),
+                y
+            )
+        }
+
 
         companion object {
             fun parse(line: String): Sensor {
@@ -63,11 +109,43 @@ class Day15 : IAocTaskKt {
             }
         }
     }
+
     data class Point(val x: Int, val y: Int) {
         override fun toString(): String {
             return "($x, $y)"
         }
 
         fun manhattan(other: Point): Int = abs(other.x - x) + abs(other.y - y)
+        fun getTuningFrequency(): Long {
+            return 4000000L * x.toLong() + y.toLong()
+        }
     }
+
+    data class RowRange(var fromX: Int, var toX: Int, val y: Int) {
+        fun extendWith(pos: Point) {
+
+        }
+
+        fun overlaps(other: RowRange): Boolean {
+            // (StartA <= EndB) and (EndA >= StartB)
+            val minB = other.fromX
+            val maxB = other.toX
+            val minA = fromX
+            val maxA = toX
+            return minA <= maxB && maxA >= minB
+        }
+
+
+        companion object {
+            fun empty(): RowRange {
+                return RowRange(-1, -1, -1)
+            }
+        }
+    }
+}
+
+private fun List<Day15.RowRange>.toRange(): Day15.RowRange {
+    val minX = this.map { it.fromX }.minOf { it }
+    val maxX = this.map { it.toX }.maxOf { it }
+    return Day15.RowRange(minX, maxX, this[0].y)
 }
