80f2145d99142290f5f9efacfd91ec5fe1642b27
kasptom <kasptom@gmail.com>
2023-12-03 07:36:21 +0100

    (Day 03) Gear Ratios - pt 2

--- src/main/kotlin/year2023/Day03.kt
+++ src/main/kotlin/year2023/Day03.kt
@@ -6,89 +6,109 @@ class Day03 : IAocTaskKt {
     override fun getFileName(): String = "aoc2023/input_03.txt"
 
     override fun solvePartOne(lines: List<String>) {
-        val schematic: Array<Array<String>> = lines.map { it.trim().split("")
-            .filter { x -> x != "" }
-            .toTypedArray() }
-            .toTypedArray()
-
+        val schematic = EngineSchematic(lines.parse2dArray())
         val numbers: List<SchematicNumber> = schematic.findNumbers()
+
         val partNumbers = numbers
-            .filter { it.isPartNumber(schematic) }
+            .onEach { it.findPart(schematic) }
+            .filter { it.partPos != null }
 
-        println(numbers.size)
-        println(numbers)
-
-        val notPartNumbers = numbers.filter { it.isPartNumber(schematic).not() }
-        println(notPartNumbers.size)
-        println(notPartNumbers)
-
-        println(partNumbers.size)
-        println(partNumbers)
-        println(partNumbers.sumOf { it.value })
+        println(partNumbers.sumOf(SchematicNumber::value))
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        TODO("Not yet implemented")
+        val schematic = EngineSchematic(lines.parse2dArray())
+        val numbers: List<SchematicNumber> = schematic.findNumbers()
+
+        val ratioNumbersSum = numbers
+            .onEach { it.findPart(schematic) }
+            .filter { it.isGearPartNumber(schematic) }
+            .groupBy { it.partPos }
+            .filterValues { it.size == 2 }
+            .values
+            .sumOf { gearNumbers -> gearNumbers.map { it.value }.reduce { x, y -> x * y } }
+
+        println(ratioNumbersSum)
     }
 
-    data class SchematicNumber(val value: Int, val minX: Int, val maxX: Int, val y: Int) {
-        fun isPartNumber(schematic: Array<Array<String>>): Boolean {
-            for (x in minX..maxX) {
+    private fun List<String>.parse2dArray() = map {
+        it.trim()
+            .split("")
+            .filter(String::isNotEmpty)
+    }
+
+    data class Pos(val x: Int, val y: Int) {
+        override fun toString(): String = "($x, $y)"
+        operator fun plus(other: Pos): Pos = Pos(x + other.x, y + other.y)
+        operator fun minus(other: Pos): Pos = Pos(x - other.x, y - other.y)
+    }
+
+    data class SchematicNumber(val value: Int, val minPos: Pos, val maxPos: Pos, var partPos: Pos? = null) {
+        fun findPart(schematic: EngineSchematic) {
+            for (x in minPos.x..maxPos.x) {
                 for (idx in DX.indices) {
-                    val dx = DX[idx]
-                    val dy = DY[idx]
-                    val xPos = x + dx
-                    val yPos = y + dy
-                    if (notInRange(xPos, schematic[0].size) || notInRange(yPos, schematic.size)) {
+                    val neighbourPos = Pos(x + DX[idx], minPos.y + DY[idx])
+                    if (schematic.notInRange(neighbourPos)) {
                         continue
                     }
-                    val neighbour = schematic[yPos][xPos]
-                    if (neighbour.matches(Regex("\\d")).not() && neighbour != ".") {
-                        return true
+                    val neighbour = schematic.cell(neighbourPos)
+                    if (neighbour.isDigit().not() && neighbour != ".") {
+                        partPos = neighbourPos
                     }
                 }
             }
-            return false
         }
 
-        private fun notInRange(pos: Int, size: Int): Boolean = pos < 0 || pos >= size
-        override fun toString(): String {
-            return "($value, x=[$minX..$maxX], y=$y)"
-        }
+        fun isGearPartNumber(schematic: EngineSchematic): Boolean =
+            if (partPos == null) false else schematic.cell(partPos!!) == "*"
 
+        override fun toString(): String = "($value, [$minPos..$maxPos])"
 
         companion object {
-            fun create(schematic: Array<Array<String>>, xIdx: Int, yIdx: Int): SchematicNumber {
-                val row = schematic[yIdx]
-                var numberBeginningIdx = xIdx
-                while (numberBeginningIdx >= 0 && row[numberBeginningIdx].matches(Regex("\\d"))) {
-                    numberBeginningIdx--
+            fun create(schematic: EngineSchematic, lastDigitPos: Pos): SchematicNumber {
+                val row = schematic.row(lastDigitPos.y)
+                var firstDigitPos = lastDigitPos
+                while (schematic.inRange(firstDigitPos) && schematic.cell(firstDigitPos).isDigit()) {
+                    firstDigitPos -= Pos(1, 0)
                 }
-                numberBeginningIdx++
-                val value = row.toList().subList(numberBeginningIdx, xIdx + 1).joinToString("").toInt()
-                return SchematicNumber(value, numberBeginningIdx, xIdx, yIdx)
+                firstDigitPos += Pos(1, 0)
+
+                val value = row.toList().subList(firstDigitPos.x, lastDigitPos.x + 1).joinToString("").toInt()
+                return SchematicNumber(value, firstDigitPos, lastDigitPos)
             }
         }
     }
 
-    private fun Array<Array<String>>.findNumbers(): List<SchematicNumber> {
-        val foundNumbers = mutableListOf<SchematicNumber>()
-        for (yIdx in indices) {
-            for (xIdx in this[yIdx].indices) {
-                if (this[yIdx][xIdx].matches(Regex("\\d"))
-                    && (xIdx == this[yIdx].size - 1 || this[yIdx][xIdx + 1].matches(Regex("\\d")).not())
-                ) {
-                    val number = SchematicNumber.create(this, xIdx, yIdx)
-                    foundNumbers.add(number)
+    class EngineSchematic(private val rawSchematic: List<List<String>>) {
+        private val positions = rawSchematic.flatMapIndexed { yIdx, row -> row.indices.map { Pos(it, yIdx) } }
+
+        fun findNumbers(): List<SchematicNumber> {
+            val foundNumbers = mutableListOf<SchematicNumber>()
+            positions.forEach {
+                val (xIdx, yIdx) = it
+                val nextPosition = it + Pos(1, 0)
+                if (isDigit(it) && (notInRange(nextPosition) || isNotDigit(nextPosition))) {
+                    val lastDigitPos = Pos(xIdx, yIdx)
+                    val number = SchematicNumber.create(schematic = this, lastDigitPos)
+                    foundNumbers += number
                 }
             }
+            return foundNumbers
         }
-        return foundNumbers
+
+        fun cell(pos: Pos): String = rawSchematic[pos.y][pos.x]
+        fun row(yIdx: Int): List<String> = rawSchematic[yIdx]
+        fun notInRange(pos: Pos): Boolean = inRange(pos).not()
+        fun inRange(pos: Pos): Boolean = pos.x in rawSchematic[0].indices && pos.y in rawSchematic.indices
+
+        private fun isDigit(pos: Pos) = cell(pos).isDigit()
+        private fun isNotDigit(pos: Pos) = isDigit(pos).not()
     }
 
     companion object {
-        val DX: IntArray = intArrayOf(-1,  0,  1, -1, 1, -1, 0, 1)
-        val DY: IntArray = intArrayOf(-1, -1, -1,  0, 0,  1, 1, 1)
+        val DX: IntArray = intArrayOf(-1, 0, 1, -1, 1, -1, 0, 1)
+        val DY: IntArray = intArrayOf(-1, -1, -1, 0, 0, 1, 1, 1)
     }
 }
 
+private fun String.isDigit(): Boolean = matches(Regex("\\d"))
