30cdeaf8a1ad2bc16be22c1db17d688dbfbad1c6
kasptom <kasptom@gmail.com>
2023-12-05 18:24:15 +0100

    (Day 05) If You Give A Seed A Fertilizer - try with ranges pt 2 - wrong

--- src/main/kotlin/year2023/Day05.kt
+++ src/main/kotlin/year2023/Day05.kt
@@ -3,17 +3,17 @@ package year2023
 import aoc.IAocTaskKt
 
 class Day05 : IAocTaskKt {
-    override fun getFileName(): String = "aoc2023/input_05.txt"
+    override fun getFileName(): String = "aoc2023/input_05_test.txt"
 
     override fun solvePartOne(lines: List<String>) {
         val seeds = lines.first().replace("seeds: ", "")
             .split(" ")
-            .map { it.toLong() }
+            .map { Range(it.toLong(), it.toLong()) }
 
         println(lowestLocationNumber(lines, seeds))
     }
 
-    private fun lowestLocationNumber(lines: List<String>, seeds: List<Long>): Long {
+    private fun lowestLocationNumber(lines: List<String>, seeds: List<Range>): Long {
         val seedToSoilIdx = lines.indexOfFirst { it.startsWith("seed-to-soil") }
         val soilToFertilizerIdx = lines.indexOfFirst { it.startsWith("soil-to-fertilizer") }
         val fertilizerToWaterIdx = lines.indexOfFirst { it.startsWith("fertilizer-to-water") }
@@ -31,23 +31,16 @@ class Day05 : IAocTaskKt {
         val humidityToLocation = lines.createConversions(humidityToLocationIdx, lines.size + 1)
 
         val seedToLocation = seeds.map { seed ->
-            val soilIdx = seedToSoil.findBySourceRange(seed)?.getDestinationFromSourceIdx(seed) ?: seed
-            val fertilizerIdx =
-                soilToFertilizer.findBySourceRange(soilIdx)?.getDestinationFromSourceIdx(soilIdx) ?: soilIdx
-            val waterIdx =
-                fertilizerToWater.findBySourceRange(fertilizerIdx)?.getDestinationFromSourceIdx(fertilizerIdx)
-                    ?: fertilizerIdx
-            val lightIdx = waterToLight.findBySourceRange(waterIdx)?.getDestinationFromSourceIdx(waterIdx) ?: waterIdx
-            val temperatureIdx =
-                lightToTemperature.findBySourceRange(lightIdx)?.getDestinationFromSourceIdx(lightIdx) ?: lightIdx
-            val humidityIdx =
-                temperatureToHumidity.findBySourceRange(temperatureIdx)?.getDestinationFromSourceIdx(temperatureIdx)
-                    ?: temperatureIdx
-            val location = humidityToLocation.findBySourceRange(humidityIdx)?.getDestinationFromSourceIdx(humidityIdx)
-                ?: humidityIdx
+            val soilIdx = seedToSoil.findBySourceRange(listOf(seed)).mapToNewRange(listOf(seed))
+            val fertilizerIdx = soilToFertilizer.findBySourceRange(soilIdx).mapToNewRange(soilIdx)
+            val waterIdx = fertilizerToWater.findBySourceRange(fertilizerIdx).mapToNewRange(fertilizerIdx)
+            val lightIdx = waterToLight.findBySourceRange(waterIdx).mapToNewRange(waterIdx)
+            val temperatureIdx = lightToTemperature.findBySourceRange(lightIdx).mapToNewRange(lightIdx)
+            val humidityIdx = temperatureToHumidity.findBySourceRange(temperatureIdx).mapToNewRange(temperatureIdx)
+            val location = humidityToLocation.findBySourceRange(humidityIdx).mapToNewRange(humidityIdx)
             location
         }
-        return seedToLocation.minOf { it }
+        return seedToLocation.minOf { it.minOf(Range::from) }
     }
 
     override fun solvePartTwo(lines: List<String>) {
@@ -57,11 +50,18 @@ class Day05 : IAocTaskKt {
             .windowed(2, 2)
             .map {
                 val (start, rangeLength) = it
-                start until (start + rangeLength)
-            }.flatMap { range -> range.map { it } }
+                Range(start, start + rangeLength)
+            }
 
-        println(seeds.size)
         println(lowestLocationNumber(lines, seeds))
+    }
+
+    data class Range(val from: Long, val to: Long)
+
+    private fun List<Conversion>.mapToNewRange(ranges: List<Range>): List<Range> {
+        return ranges.flatMap {range ->
+            this.map { it.mapToNewRange(range) }.ifEmpty { listOf(range) }
+        }
     }
 
     data class Conversion(val source: Long, val destination: Long, val rangeSize: Long) {
@@ -70,47 +70,18 @@ class Day05 : IAocTaskKt {
         private val minDest = destination
         private val maxDest = destination + rangeSize - 1
 
-        fun getOffset(): Long = destination - source
-
-        fun isInSourceRange(resourceIdx: Long): Boolean {
-            return resourceIdx in minSource..maxSource
-        }
-
-        fun overlaps(prevConversion: Conversion): Boolean {
-            return minSource <= prevConversion.maxDest && prevConversion.minDest <= maxSource
+        fun intersectsWith(range: Range): Boolean {
+            return minSource <= range.from && maxSource >= range.to
         }
 
         override fun toString(): String {
             return "(src=$minSource..$maxSource, dst=$minDest..$maxDest)"
         }
 
-        fun getDestinationFromSourceIdx(idx: Long): Long = minDest + (idx - minSource)
-
-        fun mergeWith(overlapping: List<Conversion>): List<Conversion> {
-            if (overlapping.isEmpty()) return overlapping
-            if (overlapping.size == 1) {
-                val last = overlapping.last()
-                val gapStart = if (this.source < last.source) minSource else last.maxSource + 1
-                val gapEnd = if (this.source < last.source) last.minSource - 1 else maxSource
-                val gap = Conversion(gapStart, gapStart, gapEnd - gapStart)
-                return listOf(last, gap).sortedBy { it.source }
-            }
-            val current = this
-            val toAdd = mutableListOf<Conversion>()
-                overlapping.windowed(2).forEach {
-                (prev, next) -> val rangeSize = prev.source - next.source
-                    if (rangeSize >= 1) {
-                    val gap = Conversion(prev.source + 1, prev.source + 1, rangeSize)
-                    // This may be buggy
-                    if (gap.overlaps(current)) {
-                        toAdd += gap
-                    }
-                }
-            }
-            return (overlapping + toAdd).sortedBy { it.source }
+        fun mapToNewRange(range: Range): Range {
+            val (from, to) = range
+            return Range(minDest + (from - minSource), minDest + (to - minSource))
         }
-
-        fun contains(other: Conversion): Boolean = minSource <= other.minSource && other.maxSource <= maxSource
 
         companion object {
             fun parse(line: String): Conversion {
@@ -123,20 +94,7 @@ class Day05 : IAocTaskKt {
     private fun List<String>.createConversions(thisIdx: Int, nextIdx: Int) = this.subList(thisIdx + 1, nextIdx - 1)
         .map(Conversion::parse)
 
-    private fun List<Conversion>.findBySourceRange(resourceIdx: Long): Conversion? =
-        this.firstOrNull { it.isInSourceRange(resourceIdx) }
+    private fun List<Conversion>.findBySourceRange(ranges: List<Range>): List<Conversion> =
+        ranges.flatMap { range -> this.filter { it.intersectsWith(range) } }
 
-    private fun List<Conversion>.findBySourceRange(conversion: Conversion): Conversion =
-        this.first { it.overlaps(conversion) }
-
-    private fun List<Conversion>.mapFrom(previous: List<Conversion>): List<Conversion> {
-        val merged = (this + previous).sortedBy { it.source }.fold(emptyList<Conversion>()) { acc, next ->
-            if (acc.isEmpty()) acc + next
-            else {
-                val (overlapping, notOverlapping) = acc.partition { it.overlaps(next) && !it.contains(next) }
-                (notOverlapping + next.mergeWith(overlapping)).sortedBy { it.source }
-            }
-        }
-        return merged
-    }
 }
