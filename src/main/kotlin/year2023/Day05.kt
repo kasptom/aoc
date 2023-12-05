0e884d95a59747887c7ac49b3a5f950c7a2ef3e4
kasptom <kasptom@gmail.com>
2023-12-05 20:30:55 +0100

    (Day 05) If You Give A Seed A Fertilizer - probably works (too slow)

--- src/main/kotlin/year2023/Day05.kt
+++ src/main/kotlin/year2023/Day05.kt
@@ -3,7 +3,7 @@ package year2023
 import aoc.IAocTaskKt
 
 class Day05 : IAocTaskKt {
-    override fun getFileName(): String = "aoc2023/input_05_test.txt"
+    override fun getFileName(): String = "aoc2023/input_05.txt"
 
     override fun solvePartOne(lines: List<String>) {
         val seeds = lines.first().replace("seeds: ", "")
@@ -31,13 +31,14 @@ class Day05 : IAocTaskKt {
         val humidityToLocation = lines.createConversions(humidityToLocationIdx, lines.size + 1)
 
         val seedToLocation = seeds.map { seed ->
-            val soilIdx = seedToSoil.findBySourceRange(listOf(seed)).mapToNewRange(listOf(seed))
-            val fertilizerIdx = soilToFertilizer.findBySourceRange(soilIdx).mapToNewRange(soilIdx)
-            val waterIdx = fertilizerToWater.findBySourceRange(fertilizerIdx).mapToNewRange(fertilizerIdx)
-            val lightIdx = waterToLight.findBySourceRange(waterIdx).mapToNewRange(waterIdx)
-            val temperatureIdx = lightToTemperature.findBySourceRange(lightIdx).mapToNewRange(lightIdx)
-            val humidityIdx = temperatureToHumidity.findBySourceRange(temperatureIdx).mapToNewRange(temperatureIdx)
-            val location = humidityToLocation.findBySourceRange(humidityIdx).mapToNewRange(humidityIdx)
+            val soilIdx = seedToSoil.findAffectingConversions(listOf(seed)).mapToNewRange(listOf(seed))
+            val fertilizerIdx = soilToFertilizer.findAffectingConversions(soilIdx).mapToNewRange(soilIdx)
+            val waterIdx = fertilizerToWater.findAffectingConversions(fertilizerIdx).mapToNewRange(fertilizerIdx)
+            val lightIdx = waterToLight.findAffectingConversions(waterIdx).mapToNewRange(waterIdx)
+            val temperatureIdx = lightToTemperature.findAffectingConversions(lightIdx).mapToNewRange(lightIdx)
+            val humidityIdx =
+                temperatureToHumidity.findAffectingConversions(temperatureIdx).mapToNewRange(temperatureIdx)
+            val location = humidityToLocation.findAffectingConversions(humidityIdx).mapToNewRange(humidityIdx)
             location
         }
         return seedToLocation.minOf { it.minOf(Range::from) }
@@ -56,17 +57,61 @@ class Day05 : IAocTaskKt {
         println(lowestLocationNumber(lines, seeds))
     }
 
-    data class Range(val from: Long, val to: Long)
+    data class Range(val from: Long, val to: Long) {
+        fun divideBy(conv: Conversion): List<Range> {
+            val min = conv.minSource
+            val max = conv.maxSource
+            // from, min = to, max
+            if (to == min) {
+                return listOf(Range(from, to - 1), conv.mapToNewRange(Range(to, to)))
+            }
+            // from < min < to < max
+            if (from < min && to < max) {
+                return listOf(Range(from, min - 1), conv.mapToNewRange(Range(min, to)))
+            }
+            // min <= from < to <= max
+            if (min <= from && to < max) {
+                return listOf(conv.mapToNewRange(this))
+            }
+            // min <= from <= max < to
+            if (min <= from && max < to) {
+                return listOf(conv.mapToNewRange(Range(from, max)), Range(max + 1, to))
+            }
+
+            // max == from == to
+            if (from == max && from == to) {
+                return listOf(conv.mapToNewRange(Range(from, from)))
+            }
+
+            // max == from < to
+            if (from == max) {
+                return listOf(conv.mapToNewRange(this))
+            }
+
+            return listOf(conv.mapToNewRange(Range(from, max)), Range(max + 1, to))
+
+//            return TODO("$conv $this")
+        }
+    }
 
     private fun List<Conversion>.mapToNewRange(ranges: List<Range>): List<Range> {
-        return ranges.flatMap {range ->
-            this.map { it.mapToNewRange(range) }.ifEmpty { listOf(range) }
+        val intersectingConversions = this
+        return ranges.flatMap { range ->
+            val rangeConversions = intersectingConversions.filter { conv -> conv.intersectsWith(range) }
+            if (rangeConversions.isEmpty()) {
+                listOf(range)
+            } else { // cut the range
+                rangeConversions.flatMap { conv ->
+                    val dividedRange = range.divideBy(conv)
+                    dividedRange
+                }
+            }
         }
     }
 
     data class Conversion(val source: Long, val destination: Long, val rangeSize: Long) {
-        private val minSource = source
-        private val maxSource = source + rangeSize - 1
+        val minSource = source
+        val maxSource = source + rangeSize - 1
         private val minDest = destination
         private val maxDest = destination + rangeSize - 1
 
@@ -90,6 +135,6 @@ class Day05 : IAocTaskKt {
     private fun List<String>.createConversions(thisIdx: Int, nextIdx: Int) = this.subList(thisIdx + 1, nextIdx - 1)
         .map(Conversion::parse)
 
-    private fun List<Conversion>.findBySourceRange(ranges: List<Range>): List<Conversion> =
+    private fun List<Conversion>.findAffectingConversions(ranges: List<Range>): List<Conversion> =
         ranges.flatMap { range -> this.filter { it.intersectsWith(range) } }
 }
