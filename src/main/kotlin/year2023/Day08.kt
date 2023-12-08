4f570f67cb4ee256c75dca77135a0f99ad222718
kasptom <kasptom@gmail.com>
2023-12-08 06:26:08 +0100

    (Day 08) Haunted Wasteland - part 2

--- src/main/kotlin/year2023/Day08.kt
+++ src/main/kotlin/year2023/Day08.kt
@@ -3,7 +3,6 @@ package year2023
 import aoc.IAocTaskKt
 
 class Day08 : IAocTaskKt {
-//    override fun getFileName(): String = "aoc2023/input_08.txt"
     override fun getFileName(): String = "aoc2023/input_08.txt"
 
     override fun solvePartOne(lines: List<String>) {
@@ -12,7 +11,7 @@ class Day08 : IAocTaskKt {
         val map = nodes.groupBy { it.id }
             .mapValues { (_, v) -> v.first() }
         var steps = 0;
-        var nodeId = "AAA"
+        var nodeId = nodes.first { it.id == "AAA" }.id
         var stepId = 0
 
         while (nodeId != "ZZZ") {
@@ -27,7 +26,49 @@ class Day08 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        println("second")
+        val stepsLoop = lines.first().split("").filter { it.isNotEmpty() }
+        val nodes = lines.subList(2, lines.size).map(Node::parse)
+        val map = nodes.groupBy { it.id }
+            .mapValues { (_, v) -> v.first() }
+        val nodeIds = nodes.filter { it.id.endsWith("A") }
+            .map { it.id }
+            .toMutableList()
+
+        val neededSteps = nodeIds.map {
+            var stepId = 0
+            var steps = 0
+            var nodeId = it
+            while (nodeId.endsWith("Z").not()) {
+                    val node = map[nodeId]!!
+                    nodeId = if (stepsLoop[stepId] == "L") {
+                        node.left
+                    } else node.right
+
+                steps++
+                stepId = if (stepId == stepsLoop.size - 1) 0 else stepId + 1
+            }
+            steps
+        }
+        val primeFactors = neededSteps.flatMap { stepCount -> getPrimeFactors(stepCount) }.toSet()
+        println(primeFactors)
+        val result = primeFactors.fold(1L) { x, y -> x * y}
+        println(result)
+    }
+
+    private fun getPrimeFactors(number: Int): Set<Long> {
+        val factors = mutableSetOf<Long>()
+        var factor = 1L
+        factors.add(factor)
+        factor = 2L
+        var remaining: Long =  number.toLong()
+        while (remaining > 1) {
+            if (remaining % factor == 0L) {
+                factors += factor
+                remaining /= factor
+            }
+            factor = if (factor == 2L) 3L else factor + 2L
+        }
+        return factors
     }
 
     data class Node(val id: String, val left: String, val right: String) {
