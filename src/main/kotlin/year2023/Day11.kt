94964506030e7514b1d5903e4caafc80de4993d9
kasptom <kasptom@gmail.com>
2023-12-11 06:25:27 +0100

    (Day 11) Cosmic Expansion - part 2

--- src/main/kotlin/year2023/Day11.kt
+++ src/main/kotlin/year2023/Day11.kt
@@ -2,6 +2,8 @@ package year2023
 
 import aoc.IAocTaskKt
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 
 class Day11 : IAocTaskKt{
@@ -32,6 +34,42 @@ class Day11 : IAocTaskKt{
 
         galaxies.print()
 
+        val points = findGalaxies(galaxies)
+        println(points)
+        val sum = points.sumOf { point -> points.sumOf { other -> point.dist(other) }} / 2
+        println(sum)
+    }
+
+    data class Point(val x: Int, val y: Int) {
+        fun dist(other: Point): Int = abs(x - other.x) + abs(y - other.y)
+        fun dist2(other: Point, emptyCols: List<Int>, emptyRows: List<Int>, multiplier: Long = 10000): Long {
+            val startX = min(x, other.x)
+            val endX = max(x, other.x)
+            val startY = min(y, other.y)
+            val endY = max(y, other.y)
+            val colsCount = emptyCols.count { it in startX..endX }
+            val rowsCount = emptyRows.count { it in startY..endY }
+            return abs(x - other.x) + abs(y - other.y) - (colsCount + rowsCount) + (colsCount + rowsCount) * multiplier
+        }
+    }
+
+    override fun solvePartTwo(lines: List<String>) {
+        val galaxies: MutableList<MutableList<String>> = lines.map { it.split("").filter(String::isNotEmpty).toMutableList() }
+            .toMutableList()
+        val columnsWithNoGalaxies = galaxies.indices
+            .filter { idx -> galaxies.map { it[idx] }.all { it == "." } }
+
+        val rowsWithNoGalaxies = galaxies.indices.filter { galaxies[it].all { it == "." } }
+
+        galaxies.print()
+
+        val points = findGalaxies(galaxies)
+        println(points)
+        val sum = points.sumOf { point -> points.sumOf { other -> point.dist2(other, columnsWithNoGalaxies, rowsWithNoGalaxies,  1000000) }} / 2L
+        println(sum)
+    }
+
+    private fun findGalaxies(galaxies: MutableList<MutableList<String>>): MutableList<Point> {
         val points = mutableListOf<Point>()
         for (y in galaxies.indices) {
             for (x in galaxies.first().indices) {
@@ -41,17 +79,7 @@ class Day11 : IAocTaskKt{
                 }
             }
         }
-        println(points)
-        val sum = points.sumOf { point -> points.sumOf { other -> point.dist(other) }} / 2
-        println(sum)
-    }
-
-    data class Point(val x: Int, val y: Int) {
-        fun dist(other: Point): Int = abs(x - other.x) + abs(y - other.y)
-    }
-
-    override fun solvePartTwo(lines: List<String>) {
-        println("second aa")
+        return points
     }
 
     private fun List<List<String>>.print() {
