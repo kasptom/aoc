f831ccd691328e706e0f66a98163d0f319e20be8
kasptom <kasptom@gmail.com>
2023-12-12 07:54:48 +0100

    (Day 12) Hot Springs - part 2

--- src/main/kotlin/year2023/Day12.kt
+++ src/main/kotlin/year2023/Day12.kt
@@ -1,108 +1,83 @@
 package year2023
 
 import aoc.IAocTaskKt
+import kotlin.math.min
 
 class Day12 : IAocTaskKt {
     override fun getFileName() = "aoc2023/input_12.txt"
 
     override fun solvePartOne(lines: List<String>) {
         val conditions = lines
-//            .filter { it.startsWith("?#?#?#?#?#?#?#? ") }
             .map(Condition::parse)
 
-        val configsCounts = conditions.map { it.configsCount() }
-
-        conditions.zip(configsCounts)
-            .onEach { (condition, count) -> println("$condition --> $count")}
-
-        println(configsCounts.sumOf { it })
+        println(conditions.sumOf { it.configsCount() })
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        println("second")
+        val conditions = lines
+            .map(Condition::parse2)
+
+        println(conditions.sumOf { it.configsCount() })
     }
 
     data class Condition(val springs: List<String>, val sizes: List<Int>) {
+        private val configToSizes = mutableMapOf<Pair<List<String>, List<Int>>, Long>()
         private val joined = springs.joinToString("")
 
-        fun configsCount(): Int {
-            if (springs.all { it != "?" }) return 1
-            val currentIdx = 0
-            if (springs[currentIdx] == "?") {
-                val firstVariant = springs.toMutableList()
-                val secondVariant = springs.toMutableList()
-                firstVariant[currentIdx] = "#"
-                secondVariant[currentIdx] = "."
-                return countConfigs(currentIdx + 1, firstVariant, sizes) +
-                        countConfigs(currentIdx + 1, secondVariant, sizes)
+        fun configsCount(): Long = configsCount(springs, sizes, "")
+
+        private fun configsCount(springs: List<String>, sizes: List<Int>, result: String): Long {
+            if (springs.isEmpty()) {
+                return if (sizes.isEmpty()) 1L else 0
+            } else if (sizes.isEmpty()) {
+                return if ("#" !in springs) 1L else 0L
             }
-            return countConfigs(currentIdx + 1, springs.toMutableList(), sizes)
+            val key = Pair(springs, sizes)
+            if (key in configToSizes.keys) {
+                return configToSizes[key]!!
+            }
+            var count = 0L
+            val nextSymbol = springs.first()
+            val nextSize = sizes.first()
+
+            if (nextSymbol == "." || nextSymbol == "?") {
+                count += configsCount(springs.subList(1, springs.size), sizes, "$result.")
+            }
+            val nextChunk = springs.subList(0, min(nextSize, springs.size))
+            if ("." !in nextChunk && nextChunk.size == nextSize) {
+                if (nextSize == springs.size) {
+                    count += configsCount(emptyList(), sizes.subList(1, sizes.size), "$result#")
+                } else if (nextSize < springs.size && springs.subList(nextSize, nextSize + 1).first() != "#") {
+                    val nextSprings = springs.subList(nextSize + 1, springs.size)
+                    count += configsCount(nextSprings, sizes.subList(1, sizes.size), "$result#")
+                }
+            }
+
+            configToSizes[key] = count
+            return count
         }
 
-        private fun countConfigs(currentIdx: Int, partiallyFixedSprings: MutableList<String>, sizes: List<Int>): Int {
-//            println("partially fixed: $partiallyFixedSprings")
-            val groupCounts = partiallyFixedSprings.groupsToFirstQuestionMark()
-            if (partiallyFixedSprings.all { it != "?" }) {
-                return if (groupCounts != sizes) 0
-                else 1
-            }
-            if (groupCounts.size > sizes.size) return 0
-//            if (groupCounts != sizes.subList(0, groupCounts.size)) return 0
 
-            if (partiallyFixedSprings[currentIdx] == "?") {
-                val firstVariant = partiallyFixedSprings.toMutableList()
-                val secondVariant = partiallyFixedSprings.toMutableList()
-                firstVariant[currentIdx] = "#"
-                secondVariant[currentIdx] = "."
-                return countConfigs(currentIdx + 1, firstVariant, sizes) +
-                        countConfigs(currentIdx + 1, secondVariant, sizes)
-            }
-            return countConfigs(currentIdx + 1, partiallyFixedSprings, sizes)
-        }
-
-        override fun toString(): String {
-            return "($joined, $sizes)"
-        }
+        override fun toString(): String = "($joined, $sizes)"
 
 
         companion object {
-            @JvmStatic
-            fun main(args: Array<String>) {
-                println(listOf("#", ".", "#", "#", "?").groupsToFirstQuestionMark())
-                println(listOf("#", ".", "#", "#", "#").groupsToFirstQuestionMark())
-                println(listOf("#", ".", "?", ".", "#", "#", "#").groupsToFirstQuestionMark())
-            }
-
             fun parse(line: String): Condition {
                 val (springRaw, sizesRaw) = line.split(" ")
                 val spring = springRaw.split("").filter(String::isNotEmpty)
                 val sizes = sizesRaw.split(",").filter(String::isNotEmpty).map(String::toInt)
                 return Condition(spring, sizes)
             }
+
+            fun parse2(line: String): Condition {
+                val (springRaw, sizesRaw) = line.split(" ")
+                val spring = springRaw.split("").filter(String::isNotEmpty)
+                val sizes = sizesRaw.split(",").filter(String::isNotEmpty).map(String::toInt)
+                val bigSpring = (1..4).flatMap { spring + "?" } + spring
+                val bigSizes = (1..5).flatMap { sizes }
+
+                return Condition(bigSpring, bigSizes)
+            }
         }
     }
 }
-
-fun List<String>.groupsToFirstQuestionMark(): List<Int> {
-    val groups = mutableListOf<Int>()
-    var currentGroupSize = 0
-    var lastIdx = 0
-    for (idx in indices) {
-        lastIdx = idx
-        if (this[idx] == "#") currentGroupSize++
-        if (this[idx] == "?") {
-            if (currentGroupSize != 0) groups += currentGroupSize
-            currentGroupSize = 0
-            break
-        }
-        if (this[idx] == ".") {
-            if (currentGroupSize != 0) groups += currentGroupSize
-            currentGroupSize = 0
-        }
-    }
-    if (lastIdx == size - 1 && currentGroupSize != 0) groups += currentGroupSize
-//    println("groups to sizes: $this --> $groups")
-    return groups
-}
-
-
