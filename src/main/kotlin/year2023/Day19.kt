dcdba870ce29a45b490f5f88d5b5a577e2658935
kasptom <kasptom@gmail.com>
2023-12-19 06:45:38 +0100

    (Day 19) Aplenty - part 2

--- src/main/kotlin/year2023/Day19.kt
+++ src/main/kotlin/year2023/Day19.kt
@@ -2,11 +2,14 @@ package year2023
 
 import aoc.IAocTaskKt
 import java.util.function.Predicate
+import kotlin.math.max
+import kotlin.math.min
 
 class Day19 : IAocTaskKt {
     private val labelToWorkflow: MutableMap<String, Workflow> = mutableMapOf()
 
     override fun getFileName(): String = "aoc2023/input_19.txt"
+    // 126090293754249 --> too low
 
     override fun solvePartOne(lines: List<String>) {
         val partsStartIdx = lines.indexOfFirst { it.isBlank() } + 1
@@ -39,12 +42,74 @@ class Day19 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        println("pt 2")
+        val partsStartIdx = lines.indexOfFirst { it.isBlank() } + 1
+        val workflows = lines.subList(0, partsStartIdx - 1)
+            .map(Workflow::parse)
+
+        for (workflow in workflows) {
+            labelToWorkflow[workflow.name] = workflow
+        }
+        // TODO count combinations
+        val start = "in"
+        val multiParts = mutableMapOf<List<String>, MutableList<MultiPart>>()
+        val path = listOf(start)
+        val startPart = MultiPart.start()
+
+        search(start, startPart, multiParts, path)
+
+        // test 167409079868000
+        val result = multiParts
+            .onEach { println(it) }
+            .values
+            .map { parts ->
+                parts.sumOf { it.partsCount() }
+            }
+            .sumOf { it }
+        println(result)
+    }
+
+    private fun search(
+        workflowName: String,
+        multiPart: MultiPart,
+        multiParts: MutableMap<List<String>, MutableList<MultiPart>>,
+        path: List<String>,
+    ) {
+        if (multiPart.ignore) {
+            return
+        }
+        if (workflowName == "A") {
+            println(path)
+            if (multiParts.containsKey(path)) {
+                multiParts[path]!!.add(multiPart)
+            } else {
+                multiParts[path] = mutableListOf(multiPart)
+            }
+            return
+        } else if (workflowName == "R") {
+            return
+        }
+        val workflow = labelToWorkflow[workflowName]!!
+        val nextMoves: List<Pair<String, MultiPart>> = workflow.divide(multiPart)
+        println("next moves $nextMoves")
+        for ((nextWorkflowName, nextMultiPart) in nextMoves) {
+            search(nextWorkflowName, nextMultiPart, multiParts, path + nextWorkflowName)
+        }
     }
 
     data class Workflow(val name: String, val rules: List<Rule>) {
         fun getNextWorkflowName(part: Part): String {
             return rules.first { rule -> rule.predicate.test(part) }.nextWorkflow
+        }
+
+        fun divide(multiPart: MultiPart): List<Pair<String, MultiPart>> {
+            val divided = mutableListOf<Pair<String, MultiPart>>()
+            var currentMultiPart = multiPart.copy()
+            for (rule in rules) {
+                val (accepted, notAccepted) = rule.divide(currentMultiPart)
+                divided.add(Pair(rule.nextWorkflow, accepted))
+                currentMultiPart = notAccepted
+            }
+            return divided
         }
 
         companion object {
@@ -58,10 +123,21 @@ class Day19 : IAocTaskKt {
         }
     }
 
-    data class Rule(val predicate: Predicate<Part>, val nextWorkflow: String) {
+    data class Rule(
+        val predicate: Predicate<Part>,
+        val nextWorkflow: String,
+        val category: Category?,
+        val acceptedRange: Range?,
+    ) {
+        fun divide(toDivide: MultiPart): Pair<MultiPart, MultiPart> {
+            if (category == null) {
+                return Pair(toDivide, toDivide.copy(ignore = true))
+            }
+            return toDivide.divide(category, acceptedRange!!)
+        }
+
         companion object {
             fun parse(input: String): Rule {
-                println("parsing: $input")
                 return if ("<" in input) {
                     val (category, scoreDest) = input.split("<").filter(String::isNotEmpty)
                     val (scoreRaw, dest) = scoreDest.split(":").filter(String::isNotEmpty)
@@ -73,7 +149,7 @@ class Day19 : IAocTaskKt {
                     val score = scoreRaw.toInt()
                     createFrom(category, ">", score, dest)
                 } else {
-                    Rule({ true }, input)
+                    Rule({ true }, input, null, Range.max())
                 }
             }
 
@@ -85,7 +161,8 @@ class Day19 : IAocTaskKt {
                     Category.a -> if (symbol == "<") Predicate<Part> { p -> p.a < score } else Predicate<Part> { p -> p.a > score }
                     Category.s -> if (symbol == "<") Predicate<Part> { p -> p.s < score } else Predicate<Part> { p -> p.s > score }
                 }
-                return Rule(predicate, dest)
+                val range = if (symbol == "<") Range(1, score - 1) else Range(score + 1, 4000)
+                return Rule(predicate, dest, category, range)
             }
         }
     }
@@ -106,6 +183,88 @@ class Day19 : IAocTaskKt {
                             .toInt()
                     }
                 return Part(x, m, a, s)
+            }
+        }
+    }
+
+    data class MultiPart(val x: Range, val m: Range, val a: Range, val s: Range, val ignore: Boolean = false) {
+        fun partsCount(): Long {
+            return x.size() * m.size() * a.size() * s.size()
+        }
+
+        override fun toString(): String {
+            return "(x=$x, m=$m, a=$a, s=$s)"
+        }
+
+        fun divide(category: Category, acceptedRange: Range): Pair<MultiPart, MultiPart> {
+            val rangeToUpdate = when (category) {
+                Category.x -> x
+                Category.m -> m
+                Category.a -> a
+                Category.s -> s
+            }
+            val (accepted, rejected) = if(acceptedRange.isGreaterThanRange()) {
+                Pair( // 300..4000
+                    Range(max(rangeToUpdate.from, acceptedRange.from), rangeToUpdate.to),
+                    Range(rangeToUpdate.from, min(rangeToUpdate.to, acceptedRange.from - 1))
+                )
+            } else if (acceptedRange.isLessThanRange()){
+                Pair( // 1..200
+                    Range(rangeToUpdate.from, min(rangeToUpdate.to, acceptedRange.to)),
+                    Range(max(rangeToUpdate.from, acceptedRange.to + 1), rangeToUpdate.to)
+                )
+            } else throw IllegalStateException()
+            val ignoreAccepted = accepted.isInvalid()
+            val ignoreRejected = rejected.isInvalid()
+
+            if (ignoreAccepted) println("ignore accepted $accepted")
+            if (ignoreRejected) println("ignore rejected $rejected")
+
+            return when (category) {
+                Category.x -> Pair(copy(x = accepted), copy(x = rejected))
+                Category.m -> Pair(copy(m = accepted), copy(m = rejected))
+                Category.a -> Pair(copy(a = accepted), copy(a = rejected))
+                Category.s -> Pair(copy(s = accepted), copy(s = rejected))
+            }.let {
+                val (acc, rej) = it
+                val result = Pair(acc.copy(ignore = ignoreAccepted), rej.copy(ignore = ignoreRejected))
+                println("dividing $rangeToUpdate by ($category) $acceptedRange  --> $result")
+                result
+            }
+        }
+
+
+        companion object {
+            fun start(): MultiPart {
+                return MultiPart(
+                    x = Range.max(),
+                    m = Range.max(),
+                    a = Range.max(),
+                    s = Range.max(),
+                )
+            }
+        }
+    }
+
+    data class Range(val from: Int, val to: Int) {
+        fun size(): Long = to.toLong() - from + 1
+        fun isGreaterThanRange() = to == 4000
+        override fun toString(): String {
+            return "$from..$to"
+        }
+
+        fun isInvalid(): Boolean {
+            return from > to || from < 0 || to > 4000
+        }
+
+        fun isLessThanRange(): Boolean {
+            return from == 1
+        }
+
+
+        companion object {
+            fun max(): Range {
+                return Range(1, 4000)
             }
         }
     }
