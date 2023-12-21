8c112973e5ba81f82013aa23606fe41f4039703e
kasptom <kasptom@gmail.com>
2023-12-21 07:16:12 +0100

    (Day 21) Step Counter - part 2
          Thanks to HyperNeutrino
          https://www.youtube.com/watch?v=9UOMZSL0JTg

--- src/main/kotlin/year2023/Day21.kt
+++ src/main/kotlin/year2023/Day21.kt
@@ -1,7 +1,14 @@
 package year2023
 
 import aoc.IAocTaskKt
-import year2023.Day21.Direction.*
+import year2023.Day21.Direction.DOWN
+import year2023.Day21.Direction.LEFT
+import year2023.Day21.Direction.RIGHT
+import year2023.Day21.Direction.UP
+import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
 
 class Day21 : IAocTaskKt {
     override fun getFileName(): String = "aoc2023/input_21.txt"
@@ -9,22 +16,115 @@ class Day21 : IAocTaskKt {
     override fun solvePartOne(lines: List<String>) {
         val grid = Grid.parse(lines)
         println(grid.print())
-        println(grid.visit(steps = 64).size)
-        println(grid.visited.size)
+        println(grid.visit(steps = 6).size)
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        print("Not yet implemented")
+        val grid = Grid.parse(lines)
+        println(grid.print())
+
+        val points = grid.visit(steps = 64)
+        println()
+        println(grid.print(points))
+        println(points.size)
+
+//        println(grid.countStepsInDiamond(5000))
+        // 5146892932706899 -- too high
+        // 636385126285344
+        // 636391431970648 - incorrect :/
+        println(grid.countStepsInDiamond(26501365))
+//        println(grid.countStepsInDiamond(5000))
+//        println(grid.visit(500).size) <-- test case
     }
 
     data class Grid(val grid: List<List<String>>, val start: Point) {
-        val visited = mutableSetOf<Point>()
-
         fun print(): String = grid.joinToString("\n") { it.joinToString("") }
-        fun print(points: Set<Point>): String = grid.indices.joinToString("\n") {yIdx -> grid[yIdx]
-            .indices
-            .map { xIdx -> if (Point(xIdx, yIdx) in points) "O" else grid.valueAt(Point(xIdx, yIdx))}
-            .joinToString("")
+        fun print(points: Set<Point>): String {
+            val minX = points.minOf { it.x }
+            val maxX = points.maxOf { it.x }
+            val minY = points.minOf { it.y }
+            val maxY = points.maxOf { it.y }
+
+            val width = grid[0].size
+            val height = grid.size
+
+            val megaGridMinX = -width * ceil(abs(minX).toDouble() / width).toInt()
+            val megaGridMaxX = width * ceil(maxX.toDouble() / width).toInt()
+
+            val megaGridMinY = -height * ceil(abs(minY).toDouble() / height).toInt()
+            val megaGridMaxY = height * ceil(maxY.toDouble() / height).toInt()
+
+            var result = ""
+            for (y in megaGridMinY..megaGridMaxY) {
+                for (x in megaGridMinX..megaGridMaxX) {
+                    val value = grid.valueAt(Point(x, y).moduloPoint(grid))
+//                    if (Point(x, y).isInRange(grid)) {
+//                        result += ANSI_GREEN
+//                    }
+                    result += if (Point(x, y) in points) "O" else value
+//                    if (Point(x, y).isInRange(grid)) {
+//                        result += ANSI_RESET
+//                    }
+                }
+                result += "\n"
+            }
+            return result
+        }
+
+        fun countStepsInDiamond(steps: Long): Long {
+            val size = grid[0].size
+//            require(steps % size == floor(size.toDouble() / 2).toLong()) { "no "}
+
+            val gridRepeats = floor(steps.toDouble() / size).toLong() - 1L
+
+            println(gridRepeats)
+
+            val oddRepeats = (floor(gridRepeats.toDouble() / 2) * 2 + 1).pow(2).toLong()
+            val evenRepeats = (floor((gridRepeats + 1.0) / 2) * 2).pow(2).toLong()
+
+            println("$oddRepeats $evenRepeats")
+
+            val oddCount = visit(size * 2 + 1).filter { it.isInRange(grid) }.size
+            val evenCount = visit(size * 2).filter { it.isInRange(grid) }.size
+
+            println("$oddCount $evenCount")
+
+            val topCorner = copy(start = Point(start.x, size - 1)).visit(size - 1).filter { it.isInRange(grid) }.size.toLong()
+            val rightCorner = copy(start = Point(0, start.y)).visit(size - 1).filter { it.isInRange(grid) }.size.toLong()
+            val bottomCorner = copy(start = Point(start.x, 0)).visit(size - 1).filter { it.isInRange(grid) }.size.toLong()
+            val leftCorner = copy(start = Point(size - 1, start.y)).visit(size - 1).filter { it.isInRange(grid) }.size.toLong()
+            println("$topCorner $rightCorner $bottomCorner $leftCorner")
+
+            val smallSteps = floor(size.toDouble() / 2).toInt() - 1
+            val topRight = copy(start = Point(x = 0, y = size - 1))
+                .visit(smallSteps).filter { it.isInRange(grid) }.size.toLong()
+            val bottomRight = copy(start = Point(x = size - 1, y = size - 1))
+                .visit(smallSteps).filter { it.isInRange(grid) }.size.toLong()
+            val topLeft = copy(start = Point(x = 0, y = 0))
+                .visit(smallSteps).filter { it.isInRange(grid) }.size.toLong()
+            val bottomLeft = copy(start = Point(x = size - 1, y = 0))
+                .visit(smallSteps).filter { it.isInRange(grid) }.size.toLong()
+            println("$topRight $bottomRight $topLeft $bottomLeft")
+
+            val largeSteps = (floor(size.toDouble() * 3.0) / 2).toInt() - 1
+            val largeTopRight = copy(start = Point(x = 0, y = size - 1))
+                .visit(largeSteps).filter { it.isInRange(grid) }.size.toLong()
+            val largeBottomRight = copy(start = Point(x = size - 1, y = size - 1))
+                .visit(largeSteps).filter { it.isInRange(grid) }.size.toLong()
+            val largeTopLeft = copy(start = Point(x = 0, y = 0))
+                .visit(largeSteps).filter { it.isInRange(grid) }.size.toLong()
+            val largeBottomLeft = copy(start = Point(x = size - 1, y = 0))
+                .visit(largeSteps).filter { it.isInRange(grid) }.size.toLong()
+
+            println("$largeTopRight $largeBottomRight $largeTopLeft $largeBottomLeft")
+
+            return listOf(
+                oddRepeats * oddCount,
+                evenRepeats * evenCount,
+                topCorner, rightCorner, bottomCorner, leftCorner,
+                (gridRepeats + 1) * (topRight + topLeft + bottomRight + bottomLeft),
+                gridRepeats * (largeTopRight + largeTopLeft + largeBottomRight + largeBottomLeft),
+            ).sum()
         }
 
         companion object {
@@ -49,38 +149,45 @@ class Day21 : IAocTaskKt {
 
         fun visit(steps: Int): Set<Point> {
             val pos = start
-//            visited.clear()
-//            visited.add(pos)
 
             val possibleMoves = Direction.values()
                 .map { pos + it }
-                .filter { it.isInRange(grid) && it.isAvailable(grid) }
+                .filter { it.isAvailable(grid) }
                 .toSet()
 
-            return visit(possibleMoves, steps)
+            return visit(possibleMoves, 1, steps)
         }
 
-        private fun visit(possibleMoves: Set<Point>, steps: Int): Set<Point> {
-//            println("steps left: $steps")
+        private fun visit(possibleMoves: Set<Point>, step: Int, maxSteps: Int): Set<Point> {
+//            println("step: $step")
 //            println(print(possibleMoves))
 //            visited.addAll(possibleMoves)
-            if (steps == 1) {
+            if (step == maxSteps) {
                 return possibleMoves
             }
 
             val nextMoves = possibleMoves.flatMap { move ->
                 Direction.values()
                     .map { move + it }
-                    .filter { it.isInRange(grid) && it.isAvailable(grid) }
+                    .filter { it.isAvailable(grid) }
 //                    .filter { it !in visited }
             }.toSet()
 
 
-            return visit(nextMoves, steps - 1)
+            return visit(nextMoves, step + 1, maxSteps)
         }
     }
 
     data class Point(val x: Int, val y: Int) {
+        fun moduloPoint(grid: List<List<String>>): Point {
+            val width = grid[0].size
+            val height = grid.size
+            return Point(
+                if (abs(x) % width == 0) 0 else if (x < 0) width - (abs(x) % width) else x % width,
+                if (abs(y) % height == 0) 0 else if (y < 0) height - (abs(y) % height) else y % height
+            )
+        }
+
         operator fun plus(dir: Direction): Point {
             return when (dir) {
                 UP -> this + Point(0, -1)
@@ -91,14 +198,36 @@ class Day21 : IAocTaskKt {
         }
 
         operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
+
+        //        fun isInRange(grid: List<List<String>>): Boolean = x >= 0 && y >= 0 && x < grid[0].size && y < grid.size
+        fun isAvailable(grid: List<List<String>>): Boolean {
+            val moduloPoint = moduloPoint(grid)
+            //            println("point $this, modulo: $moduloPoint (w x h = $width x $height)")
+            return grid.valueAt(moduloPoint) == "." || grid.valueAt(moduloPoint) == "S"
+        }
+
         fun isInRange(grid: List<List<String>>): Boolean = x >= 0 && y >= 0 && x < grid[0].size && y < grid.size
-        fun isAvailable(grid: List<List<String>>): Boolean = grid.valueAt(this) == "." || grid.valueAt(this) == "S"
+
+        override fun toString(): String {
+            return "($x, $y)"
+        }
     }
 
     enum class Direction {
         UP, DOWN, LEFT, RIGHT;
     }
 
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(-11 % 11)
+        }
+
+        const val ANSI_RESET = "\u001B[0m"
+        const val ANSI_GREEN = "\u001B[32m"
+        const val ANSI_RED = "\u001B[31m"
+        const val ANSI_PURPLE = "\u001B[35m"
+    }
 }
 
 fun List<List<String>>.valueAt(pos: Day21.Point): String = this[pos.y][pos.x]
