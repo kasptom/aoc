a5b1082e972c8c3d367a1a8327905e7f98f35bbe
kasptom <kasptom@gmail.com>
2024-12-04 06:58:18 +0100

    (Day 04) Ceres Search - part 2 - wip

--- src/main/kotlin/year2024/Day04.kt
+++ src/main/kotlin/year2024/Day04.kt
@@ -1,37 +1,75 @@
 package year2024
 
 import aoc.IAocTaskKt
-import java.util.regex.Pattern
 
 class Day04 : IAocTaskKt {
     override fun getFileName(): String = "aoc2024/input_04_test.txt"
 
     override fun solvePartOne(lines: List<String>) {
-        val vertical = lines
-        val horizontal = lines[0].indices.map { idx ->
-            lines.map { it[idx] }.joinToString("")
+        val grid: Array<CharArray> = lines.map { it.toCharArray() }.toTypedArray()
+        var xmasCount = 0
+        for (i in grid.indices) {
+            for (j in grid[0].indices) {
+                for (dir in DX.indices) {
+                    xmasCount += if (xmasFound(DX[dir], DY[dir], j, i, grid)) 1 else 0
+                }
+            }
         }
-        val vertRev = vertical.map { it.reversed() }
-        val horRev = horizontal.map { it.reversed() }
+        println(xmasCount)
+    }
 
-        val word = Pattern.compile("XMAS")
-        val all = vertical + horizontal + vertRev + horRev
-        println(horizontal)
-        println(horRev)
-        println(vertical)
-        println(vertRev)
-        val counts = all.map { it.countWord(word) }
-        println(counts.sum())
+    private fun xmasFound(dx: Int, dy: Int, x: Int, y: Int, grid: Array<CharArray>): Boolean {
+        val word = "XMAS"
+        val width = grid[0].size
+        val height = grid.size
+        val found = word.indices.map { offset ->
+            val newX = x + dx * offset
+            val newY = y + dy * offset
+            if (newX in 0 until width && newY in 0 until height) {
+                grid[newY][newX]
+            } else {
+                '.'
+            }
+        }.joinToString("")
+        return found == word
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        if (lines.isEmpty()) println("empty lines 4") else println(lines.size)
+        val grid: Array<CharArray> = lines.map { it.toCharArray() }.toTypedArray()
+        var xmasCount = 0
+        for (i in grid.indices) {
+            for (j in grid[0].indices) {
+                xmasCount += if (xmasFound2(j, i, grid)) 1 else 0
+            }
+        }
+        println(xmasCount)
     }
-}
 
-private fun String.countWord(word: Pattern): Long {
-    val count = word.matcher(this)
-        .results()
-        .count()
-    return count
+    private fun xmasFound2(x: Int, y: Int, grid: Array<CharArray>): Boolean {
+        if (grid[y][x] != 'A') {
+            return false
+        }
+        val width = grid[0].size
+        val height = grid.size
+        var mCount = 0
+        var sCount = 0
+        for (idx in DX2.indices) {
+            val dx = DX2[idx]
+            val dy = DY2[idx]
+            val newX = x + dx
+            val newY = y + dy
+            if (newX in 0 until width && newY in 0 until height) {
+                mCount += if (grid[newY][newX] == 'M') 1 else 0
+                sCount += if (grid[newY][newX] == 'S') 1 else 0
+            }
+        }
+        return mCount == 2 && sCount == 2
+    }
+
+    companion object {
+        val DX = arrayOf(-1, 0, 1, -1, 1, -1, 0, 1)
+        val DY = arrayOf(-1, -1, -1, 0, 0, 1, 1, 1)
+        val DX2 = arrayOf(-1, 1, -1, 1)
+        val DY2 = arrayOf(-1, -1, 1, 1)
+    }
 }
