334cfa3863aa1f60d78ee9dca0e9dfcd1a5ab489
kasptom <kasptom@gmail.com>
2024-12-06 07:58:34 +0100

    (Day 06) Guard Gallivant - part 2

--- src/main/kotlin/year2024/Day06.kt
+++ src/main/kotlin/year2024/Day06.kt
@@ -3,6 +3,7 @@ package year2024
 import aoc.IAocTaskKt
 import year2024.Day06.Companion.DX
 import year2024.Day06.Companion.DY
+import year2024.Day06.Companion.IDX_TO_DIR
 import year2024.Day06.Companion.STATES
 
 class Day06 : IAocTaskKt {
@@ -23,16 +24,60 @@ class Day06 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        if (lines.isEmpty()) println("empty lines") else println(lines.size)
+        val grid: Array<CharArray> = lines.map { it.toCharArray() }.toTypedArray()
+        val pos = grid.findPosition()
+        val guard = Guard(pos, DIR_TO_IDX[grid.valueAt(pos)]!!)
+        grid.setValue(pos, '.')
+
+        var places = 0
+        for (x in grid[0].indices) {
+            for (y in grid.indices) {
+                val obstructionPos = Point(x, y)
+                if (grid.valueAt(obstructionPos) == '.' && obstructionPos != guard.initialPos) {
+                    grid.setValue(obstructionPos, 'O')
+                } else {
+                    continue
+                }
+                guard.reset()
+                while (guard.pos.isInRange(grid)) {
+                    guard.move(grid)
+//                    grid.printState(guard)
+                    if (guard.isStuck()) {
+                        places++
+                        break
+                    }
+                }
+                grid.setValue(obstructionPos, '.')
+            }
+        }
+        println(places)
     }
 
     companion object {
         val DX = arrayOf(0, 1, 0, -1)
         val DY = arrayOf(-1, 0, 1, 0)
         val DIR_TO_IDX = mapOf('^' to 0, '>' to 1, 'v' to 2, '<' to 3)
+        val IDX_TO_DIR = mapOf(0 to '^', 1 to '>', 2 to 'v', 3 to '<')
         val STATES = setOf('^', '>', 'v', '<')
     }
 
+}
+
+private fun Array<CharArray>.printState(guard: Guard) {
+    println("-----")
+    for (y in this.indices) {
+        for (x in this.indices) {
+            val point = Point(x, y)
+            if (guard.pos == point) {
+                print(IDX_TO_DIR[guard.dirIdx])
+            } else if (point in guard.visited) {
+                print("x")
+            } else {
+                print(this.valueAt(point))
+            }
+        }
+        println()
+    }
 }
 
 private fun Array<CharArray>.setValue(pos: Point, c: Char) {
@@ -54,29 +99,36 @@ data class Point(val x: Int, val y: Int) {
 }
 
 data class Guard(var pos: Point, var dirIdx: Int, val visited: MutableSet<Point> = mutableSetOf(pos)) {
-    fun move(grid: Array<CharArray>): Point {
+    var obstacleSeenCount = mutableMapOf<Point, Int>()
+    val initialPos = pos
+    private val initialDir = dirIdx
+
+    fun move(grid: Array<CharArray>) {
         val nextPos = pos.next(dirIdx)
-
         if (nextPos.isInRange(grid).not()) {
-            this.pos = nextPos
-            return pos
-        }
-
-        if (grid.valueAt(nextPos) == '.') {
+            pos = nextPos
+        } else if (grid.valueAt(nextPos) == '.') {
             visited.add(nextPos)
-            this.pos = nextPos
-            return pos
-        }
-
-        if (grid.valueAt(nextPos) == '#') {
+            pos = nextPos
+        } else if (grid.valueAt(nextPos) == '#' || grid.valueAt(nextPos) == 'O') {
+            obstacleSeenCount.putIfAbsent(nextPos, 0)
+            obstacleSeenCount[nextPos] = obstacleSeenCount[nextPos]!! + 1
             dirIdx += 1
             dirIdx %= 4
-            pos
-            return pos
+        } else {
+            throw IllegalStateException("unknown state")
         }
-        throw IllegalStateException("unknown state")
     }
 
+    fun reset() {
+        visited.clear()
+        visited.add(initialPos)
+        dirIdx = initialDir
+        pos = initialPos
+        obstacleSeenCount.clear()
+    }
+
+    fun isStuck(): Boolean = obstacleSeenCount.values.any { it > 4 }
 }
 
 private fun Array<CharArray>.findPosition(): Point {
