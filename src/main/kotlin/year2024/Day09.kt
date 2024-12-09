d61ff0c63072db79bca824b11f0886980753b86b
kasptom <kasptom@gmail.com>
2024-12-09 07:36:32 +0100

    (Day 09) Disk Fragmenter - part 2 (cleanup)

--- src/main/kotlin/year2024/Day09.kt
+++ src/main/kotlin/year2024/Day09.kt
@@ -4,49 +4,9 @@ import aoc.IAocTaskKt
 
 class Day09 : IAocTaskKt {
     override fun getFileName(): String = "aoc2024/input_09.txt"
-//     override fun getFileName(): String = "aoc2024/input_09_test.txt"
 
     override fun solvePartOne(lines: List<String>) {
-        // 2333133121414131402
-        // file length free space
-        // Each file on disk also has an ID number
-        // based on the order of the files as they appear *before they are rearranged* 0-idx
-        // 12345
-        // --> 0..111....22222
-        // 2333133121414131402
-        // --> 00...111...2...333.44.5555.6666.777.888899
-        // moving blocks
-        /* (steps)
-            0..111....22222
-            02.111....2222.
-            022111....222..
-            0221112...22...
-            02211122..2....
-            022111222......
-         */
-        // then compute checksum:
-        // multiply position time idx
-        // 2333133121414131402
-        // --> 00...111...2...333.44.5555.6666.777.888899
-        /*
-            00...111...2...333.44.5555.6666.777.888899
-            009..111...2...333.44.5555.6666.777.88889.
-            0099.111...2...333.44.5555.6666.777.8888..
-            00998111...2...333.44.5555.6666.777.888...
-            009981118..2...333.44.5555.6666.777.88....
-            0099811188.2...333.44.5555.6666.777.8.....
-            009981118882...333.44.5555.6666.777.......
-            0099811188827..333.44.5555.6666.77........
-            00998111888277.333.44.5555.6666.7.........
-            009981118882777333.44.5555.6666...........
-            009981118882777333644.5555.666............
-            00998111888277733364465555.66.............
-            0099811188827773336446555566..............
-         */
-        // 0 * 0 = 0, 1 * 0 = 0, 2 * 9 = 18, 3 * 9 = 27, 4 * 8 = 32, and so on. In this example, the checksum is the sum of these, 1928
         val result = getBlocks(lines)
-        println(result.map { if (it == -1) "." else it.toString() }.joinToString(""))
-
         var lastDigitIdx = result.size - 1
         var firstSpaceIdx = result.indexOfFirst { it == -1 }
         while (firstSpaceIdx < lastDigitIdx) {
@@ -60,9 +20,7 @@ class Day09 : IAocTaskKt {
 
     override fun solvePartTwo(lines: List<String>) {
         val result = getBlocks2(lines).toMutableList()
-        println(result.joinToString("") {it.toCode()})
 
-        // part 2
         val maxFileId = result.maxOf { it.value }
         for (fileId in maxFileId downTo 1) {
             val fileIdx = result.indexOfFirst { it.value == fileId }
@@ -81,11 +39,6 @@ class Day09 : IAocTaskKt {
                 }
             }
         }
-        println(result.joinToString(""))
-        println(result.joinToString("") { it.toCode() })
-
-        //
-
         println(result.sumOf { it.checksum() })
     }
 
@@ -169,9 +122,5 @@ data class Block(val value: Int, val fromIndex: Int, val toIndex: Int) {
 
     override fun toString(): String {
         return if (value != -1) "$fromIndex..$toIndex($value)" else "$fromIndex..$toIndex(❌)"
-    }
-
-    fun toCode(): String {
-        return (1..length).map { if (value != -1) value else "." }.joinToString("")
     }
 }
