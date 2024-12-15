4565fc40f986d10fd3e1c4a61311bc871aa097ba
kasptom <kasptom@gmail.com>
2024-12-15 08:17:27 +0100

    (Day 15) Warehouse Woes - part 2 (wip: move left)

--- src/main/kotlin/year2024/Day15.kt
+++ src/main/kotlin/year2024/Day15.kt
@@ -1,10 +1,12 @@
 package year2024
 
 import aoc.IAocTaskKt
+import utils.except
+import year2024.Day15.Item.Type
 
 class Day15 : IAocTaskKt {
-        override fun getFileName(): String = "aoc2024/input_15.txt"
-//    override fun getFileName(): String = "aoc2024/input_15_test_2.txt"
+    //        override fun getFileName(): String = "aoc2024/input_15.txt"
+    override fun getFileName(): String = "aoc2024/input_15_test_3.txt"
 
     override fun solvePartOne(lines: List<String>) {
         val separatorIdx = lines.indexOfFirst { it.isBlank() }
@@ -49,7 +51,59 @@ class Day15 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        if (lines.isEmpty()) println("empty lines") else println(lines.size)
+        val separatorIdx = lines.indexOfFirst { it.isBlank() }
+        val map: Array<CharArray> = lines.subList(0, separatorIdx)
+            .map { it.map { wider(it) }.flatten().toCharArray() }
+            .toTypedArray()
+
+        val moves = lines.subList(separatorIdx, lines.size)
+            .map { it.chunked(1) }
+            .flatten()
+
+        println(moves)
+
+        var robot: Point = findRobot(map)
+        var items: List<Item> = findItems(map)
+        map.clear()
+
+        println("Initial state:")
+        println(map.print(robot, items))
+        for (code in moves) {
+            val move = Point.fromMoveCode(code)
+            val (i, r) = items.moveItems(robot, move)
+            robot = r
+            items = i
+            println("Move $code:")
+            println(map.print(robot, items))
+        }
+
+        val gpsSum = items.gpsSum()
+        println(gpsSum)
+    }
+
+    private fun findItems(map: Array<CharArray>): List<Item> {
+        val items = mutableListOf<Item>()
+        for (y in map.indices) {
+            for (x in map[0].indices) {
+                val point = Point(x, y)
+                if (point.x % 2 == 0 && map.valueAt(point) == '#') {
+                    items += Item(point, point + Point(1, 0), Type.WALL)
+                } else if (map.valueAt(point) == '[') {
+                    items += Item(point, point + Point(1, 0), Type.BOX)
+                }
+            }
+        }
+        return items
+    }
+
+    private fun wider(symbol: Char): List<Char> {
+        return when (symbol) {
+            '#' -> listOf('#', '#')
+            '.' -> listOf('.', '.')
+            'O' -> listOf('[', ']')
+            '@' -> listOf('@', '.')
+            else -> throw IllegalArgumentException("Unknown symbol: $symbol")
+        }
     }
 
     private fun Array<CharArray>.print(robot: Point): Any {
@@ -85,6 +139,24 @@ class Day15 : IAocTaskKt {
             }
         }
 
+    }
+
+    data class Item(val left: Point, val right: Point, val type: Type) {
+        enum class Type {
+            BOX, WALL
+        }
+
+        fun mapRepresentation(idx: Int): String = when (type) {
+            Type.BOX -> "[]".substring(idx, idx + 1)
+            Type.WALL -> "##".substring(idx, idx + 1)
+        }
+
+        fun touchesX(other: Item): Boolean =
+            right + Point(1, 0)  == other.left || other.right + Point(1, 0) == this.left
+
+        fun move(move: Point): Item {
+            return copy(left = left + move, right = right + move)
+        }
     }
 
     private fun Array<CharArray>.move(robot: Point, move: Point): Point {
@@ -137,5 +209,78 @@ class Day15 : IAocTaskKt {
             this.setValue(position, view[i])
             position += move
         }
+    }
+
+    private fun Array<CharArray>.clear() {
+        for (y in this.indices) {
+            for (x in this[0].indices) {
+                this[y][x] = '.'
+            }
+        }
+    }
+
+    private fun Array<CharArray>.print(robot: Point, items: List<Item>): String {
+        var result = ""
+        for (y in this.indices) {
+            for (x in this[0].indices) {
+                val point = Point(x, y)
+                if (robot == point) {
+                    result += "@"
+                } else if (items.any { it.left == point }) {
+                    result += items.first { it.left == point }.mapRepresentation(0)
+                } else if (items.any { it.right == point} ) {
+                    result += items.first { it.right == point }.mapRepresentation(1)
+                } else {
+                    result += '.'
+                }
+            }
+            result += "\n"
+        }
+        return result
+    }
+
+    private fun List<Item>.moveItems(robot: Point, move: Point): Pair<List<Item>, Point> {
+        val nextPosition = robot + move
+
+        if (this.none { it.left == nextPosition || it.right == nextPosition }) {
+            return Pair(this, nextPosition)
+        }
+
+        if (move.x == -1) { // left shift horizontal
+            val touched = this.first { it.right == nextPosition }
+            val affected = findAffectedHorizontal(this.except(touched), touched, move)
+            if (affected.any { it.type == Type.WALL }) {
+                return Pair(this, robot)
+            }
+
+            val moved = affected.map { it.move(move) }
+            val updated = this.except(affected.toSet()) + moved
+            return Pair(updated, nextPosition)
+        }
+
+        return Pair(this, robot)
+    }
+
+    private fun findAffectedHorizontal(other: List<Item>, touched: Item, move: Point): List<Item> {
+        val toCheck = other
+            .filter { it.left.y == touched.left.y && it.left.x.compareTo(touched.left.x) == move.x }
+            .toMutableList()
+        val affected = mutableListOf(touched)
+        while (toCheck.any { affected.any { af -> af.touchesX(it) } }) {
+            val touchedItems = toCheck.filter { tc -> affected.any { af -> tc.touchesX(af) } }
+            affected.addAll(touchedItems)
+            toCheck.removeAll(touchedItems)
+        }
+        return affected
+    }
+
+    private fun List<Item>.gpsSum(): Long {
+        var sum = 0L
+        for (item in this) {
+            if (item.type == Type.BOX) {
+                sum += 100 * item.left.y + item.left.x
+            }
+        }
+        return sum
     }
 }
