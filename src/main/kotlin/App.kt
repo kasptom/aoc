77236425324463ac2d217b30da5f8459139290eb
kasptom <kasptom@gmail.com>
2024-12-16 06:54:30 +0100

    (Day 16) Reindeer Maze - part 1

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,9 @@
 import aoc.IAocTaskKt
 import utils.InputReader
-import year2024.Day15
+import year2024.Day16
 
 fun main() {
-    val task: IAocTaskKt = Day15()
+    val task: IAocTaskKt = Day16()
     val lines: List<String> = InputReader(task.getFileName())
         .readLines()
 
---
+++ src/main/kotlin/year2024/Day16.kt
@@ -0,0 +1,166 @@
+package year2024
+
+import aoc.IAocTaskKt
+import year2024.Day16.Reindeer.Companion.DIR_TO_ROTATIONS
+import year2024.Day16.Reindeer.Direction
+import year2024.Day16.Reindeer.Direction.E
+import year2024.Day16.Reindeer.Direction.N
+import year2024.Day16.Reindeer.Direction.S
+import year2024.Day16.Reindeer.Direction.W
+import java.util.*
+import kotlin.math.sqrt
+
+class Day16 : IAocTaskKt {
+    //    override fun getFileName(): String = "aoc2024/input_16.txt"
+    override fun getFileName(): String = "aoc2024/input_16.txt"
+
+    override fun solvePartOne(lines: List<String>) {
+        val grid = lines.map { it.toCharArray() }.toTypedArray()
+        val points = mutableSetOf<Point>()
+        var reindeerPoint = Point(-1, -1)
+        var target = Point(-1, -1)
+        for (y in grid.indices) {
+            for (x in grid[0].indices) {
+                val point = Point(x, y)
+                if (grid.valueAt(point) in setOf('.', 'S', 'E')) {
+                    points.add(point)
+                }
+                if (grid.valueAt(point) == 'S') {
+                    reindeerPoint = point
+                } else if (grid.valueAt(point) == 'E') {
+                    target = point
+                }
+            }
+        }
+        val reindeer = Reindeer(reindeerPoint, E)
+        val shortest: Int = shortestPath(points, reindeer, target)
+        println("shortest")
+        println(shortest)
+    }
+
+    private fun shortestPath(nodes: Set<Point>, initialReindeer: Reindeer, target: Point): Int {
+        val visited = mutableSetOf<State>()
+        val queue = PriorityQueue<StateWithCost>()
+
+        val initialState: State = initialReindeer.getState()
+        queue.add(StateWithCost(initialState, Cost(0, 0)))
+
+        while (queue.isNotEmpty()) {
+            val (state, cost) = queue.remove()
+            visited.add(state)
+
+            if (state.point == target) {
+                println(cost)
+                return cost.toValue()
+            }
+
+            val neighs: List<State> = state.point.getNeighs(visited, nodes)
+            val neighCosts: List<Cost> = neighs.map { it.toCost(cost, state) }
+            queue.addAll(neighs.zip(neighCosts).map { (n, c) ->
+                StateWithCost(n, c)
+            })
+        }
+        return -1
+    }
+
+    override fun solvePartTwo(lines: List<String>) {
+        if (lines.isEmpty()) println("empty lines") else println(lines.size)
+    }
+
+    data class StateWithCost(val state: State, val cost: Cost) : Comparable<StateWithCost> {
+        override fun compareTo(other: StateWithCost): Int {
+            if (cost.toValue() != other.cost.toValue()) {
+                return cost.toValue().compareTo(other.cost.toValue())
+            }
+            return state.compareTo(other.state)
+        }
+    }
+
+    data class Point(val x: Int, val y: Int) : Comparable<Point> {
+        fun getNeighs(visited: Set<State>, nodes: Set<Point>): List<State> {
+            val states = listOf(
+                State(this + UP, N),
+                State(this + DOWN, S),
+                State(this + LEFT, W),
+                State(this + RIGHT, E)
+            )
+            return states.filter { it !in visited && it.point in nodes }
+        }
+
+        private operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
+
+        companion object {
+            val UP = Point(0, -1)
+            val DOWN = Point(0, 1)
+            val LEFT = Point(-1, 0)
+            val RIGHT = Point(1, 0)
+        }
+
+        override fun compareTo(other: Point): Int {
+            if (dist() != other.dist()) {
+                return dist().compareTo(other.dist())
+            }
+            if (x != other.x) {
+                return Integer.compare(x, other.x)
+            }
+            return Integer.compare(y, other.y)
+        }
+
+        fun dist(): Double = sqrt(0.0 + x * x + y * y)
+    }
+
+    data class State(val point: Point, val direction: Direction) : Comparable<State> {
+        fun toCost(cost: Cost, state: State): Cost {
+            val rotations = DIR_TO_ROTATIONS.getOrDefault(Pair(state.direction, direction), 0)
+            val steps = 1
+            return cost + Cost(steps, rotations)
+        }
+
+        override fun compareTo(other: State): Int {
+            if (point != other.point) {
+                return point.compareTo(other.point)
+            }
+            return direction.compareTo(other.direction)
+        }
+    }
+
+    data class Cost(val steps: Int, val rotations: Int) {
+        fun toValue(): Int = steps + rotations * 1000
+        operator fun plus(cost: Cost): Cost = Cost(steps + cost.steps, rotations + cost.rotations)
+    }
+
+    data class Reindeer(
+        val pos: Point,
+        val direction: Direction,
+        val stepsCount: Int = 0,
+        val rotationsCount: Int = 0,
+    ) {
+        fun getState(): State = State(pos, direction)
+
+        enum class Direction {
+            N, S, W, E
+        }
+
+        companion object {
+            val DIR_TO_NEXT_CLOCKWISE = mapOf(N to E, E to S, S to W, W to N)
+            val DIR_TO_NEXT_COUNTER = mapOf(N to W, W to S, S to E, E to N)
+            val DIR_TO_ROTATIONS = mapOf(
+                Pair(N, E) to 1,
+                Pair(N, W) to 1,
+                Pair(N, S) to 2,
+                Pair(E, N) to 1,
+                Pair(E, S) to 1,
+                Pair(E, W) to 2,
+                Pair(S, E) to 1,
+                Pair(S, W) to 1,
+                Pair(S, N) to 2,
+                Pair(W, S) to 1,
+                Pair(W, N) to 1
+            )
+        }
+    }
+
+    private fun Array<CharArray>.valueAt(point: Point): Char {
+        return this[point.y][point.x]
+    }
+}
