1a6445f54096a8d09d03eca8b74f7b4701ee7d11
kasptom <kasptom@gmail.com>
2024-12-24 06:55:23 +0100

    (Day 24) Crossed Wires - part 1

--- src/main/kotlin/year2024/Day24.kt
+++ src/main/kotlin/year2024/Day24.kt
@@ -1,7 +1,6 @@
 package year2024
 
 import aoc.IAocTaskKt
-import java.util.*
 
 class Day24 : IAocTaskKt {
     override fun getFileName(): String = "aoc2024/input_24.txt"
@@ -18,7 +17,35 @@ class Day24 : IAocTaskKt {
 
         val edges = lines.subList(lines.indexOfFirst { it.isBlank() } + 1, lines.size)
             .map { Edge.parse(it) }
-        println(edges)
+
+        val wirePairToEdges = edges.groupBy { it.wirePair() }
+
+        var prevCount = 0
+
+        while (prevCount != wireToValue.size) {
+            val wires = wireToValue.keys.toList()
+            prevCount = wireToValue.size
+            for (a in wires) {
+                for (b in wires) {
+                    val wirePair = setOf(a, b)
+                    if (wirePairToEdges.containsKey(wirePair)) {
+                        val edges = wirePairToEdges[wirePair]!!
+                        for (edge in edges) {
+                            val (wire, value) = edge.compute(wireToValue)
+                            wireToValue[wire] = value
+                        }
+                    }
+                }
+            }
+        }
+        println("0011111101000".toInt(2))
+        wireToValue.filter { (k, _) -> k.startsWith("z") }
+            .keys
+            .sortedDescending()
+            .map { k -> wireToValue[k]!! }
+            .joinToString("")
+            .let { it.toLong(2) }
+            .let { println(it) }
     }
 
     override fun solvePartTwo(lines: List<String>) {
@@ -26,8 +53,25 @@ class Day24 : IAocTaskKt {
     }
 
     data class Edge(val left: String, val right: String, val operation: Operation, val dest: String) {
+        fun wirePair(): Set<String> {
+            return setOf(left, right)
+        }
+
+        fun compute(wireToValue: MutableMap<String, Int>): Pair<String, Int> {
+            val leftVal = wireToValue[left]!!
+            val rightVal = wireToValue[right]!!
+            val result = operation.compute(leftVal, rightVal)
+            return Pair(dest, result)
+        }
+
         enum class Operation {
-            XOR, OR, AND
+            XOR, OR, AND;
+
+            fun compute(leftVal: Int, rightVal: Int): Int = when (this) {
+                XOR -> leftVal.xor(rightVal)
+                OR -> leftVal.or(rightVal)
+                AND -> leftVal.and(rightVal)
+            }
         }
 
         companion object {
