7ea0b222ca1a79e69e350d8082ce842f9d08d631
kasptom <kasptom@gmail.com>
2024-12-24 08:08:06 +0100

    (Day 24) Crossed Wires - wip: part 2
          This approach may take a few hundred hours :V

--- src/main/kotlin/year2024/Day24.kt
+++ src/main/kotlin/year2024/Day24.kt
@@ -1,6 +1,7 @@
 package year2024
 
 import aoc.IAocTaskKt
+import utils.except
 
 class Day24 : IAocTaskKt {
     override fun getFileName(): String = "aoc2024/input_24.txt"
@@ -85,6 +86,57 @@ class Day24 : IAocTaskKt {
 
         val expectedZ = (x.toLong(2) + y.toLong(2)).toString(2)
         println("z = $expectedZ (expected)")
+
+        println("edges count")
+        println(wirePairToEdges.values.flatten().size)
+
+        val edges = wirePairToEdges.values.flatten()
+
+        val (initialWireToValue, initialWirePairToEdges) = parse(lines)
+        for (firstEdge in edges) {
+            for (secondEdge in edges.except(firstEdge)) {
+                for (thirdEdge in edges.except(firstEdge, secondEdge)) {
+                    for (fourthEdge in edges.except(firstEdge, secondEdge, thirdEdge)) {
+
+                        val firstWirePair = firstEdge.wirePair()
+                        val secondWirePair = secondEdge.wirePair()
+                        val thirdWirePair = thirdEdge.wirePair()
+                        val fourthWirePair = fourthEdge.wirePair()
+                        val wirePairs = (firstWirePair + secondWirePair + thirdWirePair + fourthWirePair)
+                            .sorted()
+                            .toList()
+                        if (wirePairs.size != 8) {
+                            continue
+                        }
+
+                        val updatedWirePairToEdges = initialWirePairToEdges.toMutableMap()
+                            .mapValues { (_, v) -> v.toMutableSet() }
+
+                        val (swappedFirstEdge, swappedSecondEdge) = firstEdge.swapOutput(secondEdge)
+                        updatedWirePairToEdges[firstWirePair]!!.remove(firstEdge)
+                        updatedWirePairToEdges[firstWirePair]!!.add(swappedFirstEdge)
+                        updatedWirePairToEdges[secondWirePair]!!.remove(secondEdge)
+                        updatedWirePairToEdges[secondWirePair]!!.add(swappedSecondEdge)
+
+                        val (swappedThirdEdge, swappedFourthEdge) = thirdEdge.swapOutput(fourthEdge)
+                        updatedWirePairToEdges[thirdWirePair]!!.remove(thirdEdge)
+                        updatedWirePairToEdges[thirdWirePair]!!.add(swappedThirdEdge)
+                        updatedWirePairToEdges[fourthWirePair]!!.remove(fourthEdge)
+                        updatedWirePairToEdges[fourthWirePair]!!.add(swappedFourthEdge)
+
+                        val swappedWirePairToEdges = updatedWirePairToEdges
+                            .mapValues { (_, v) -> v.toList() }
+                        compute(initialWireToValue.toMutableMap(), swappedWirePairToEdges)
+
+                        val zAfterSwap = valueOfWires(wireToValue, "z")
+                        if (zAfterSwap == expectedZ) {
+                            println(wirePairs.joinToString(","))
+                            return
+                        }
+                    }
+                }
+            }
+        }
     }
 
     data class Edge(val left: String, val right: String, val operation: Operation, val dest: String) {
@@ -97,6 +149,12 @@ class Day24 : IAocTaskKt {
             val rightVal = wireToValue[right]!!
             val result = operation.compute(leftVal, rightVal)
             return Pair(dest, result)
+        }
+
+        fun swapOutput(otherEdge: Edge): Pair<Edge, Edge> {
+            val a = copy(dest = otherEdge.dest)
+            val b = otherEdge.copy(dest = dest)
+            return Pair(a, b)
         }
 
         enum class Operation {
