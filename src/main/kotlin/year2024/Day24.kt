11a54a2f1e35469f47073dbec9712ff988467c4c
kasptom <kasptom@gmail.com>
2024-12-24 07:16:57 +0100

    (Day 24) Crossed Wires - wip: part 2

--- src/main/kotlin/year2024/Day24.kt
+++ src/main/kotlin/year2024/Day24.kt
@@ -7,19 +7,17 @@ class Day24 : IAocTaskKt {
     // override fun getFileName(): String = "aoc2024/input_24_test.txt"
 
     override fun solvePartOne(lines: List<String>) {
-        val wireToValue = mutableMapOf<String, Int>()
-        val values = lines.subList(0, lines.indexOfFirst { it.isBlank() })
-            .associate { wireValue ->
-                val (wire, value) = wireValue.split(": ")
-                Pair(wire, value.toInt())
-            }
-        wireToValue.putAll(values)
+        val (wireToValue, wirePairToEdges) = parse(lines)
+        compute(wireToValue, wirePairToEdges)
+        valueOfWires(wireToValue)
+            .let { it.toLong(2) }
+            .let { println(it) }
+    }
 
-        val edges = lines.subList(lines.indexOfFirst { it.isBlank() } + 1, lines.size)
-            .map { Edge.parse(it) }
-
-        val wirePairToEdges = edges.groupBy { it.wirePair() }
-
+    private fun compute(
+        wireToValue: MutableMap<String, Int>,
+        wirePairToEdges: Map<Set<String>, List<Edge>>,
+    ) {
         var prevCount = 0
 
         while (prevCount != wireToValue.size) {
@@ -38,18 +36,55 @@ class Day24 : IAocTaskKt {
                 }
             }
         }
-        println("0011111101000".toInt(2))
-        wireToValue.filter { (k, _) -> k.startsWith("z") }
+    }
+
+    private fun parse(lines: List<String>): Pair<MutableMap<String, Int>, Map<Set<String>, List<Edge>>> {
+        val wireToValue = mutableMapOf<String, Int>()
+        val values = lines.subList(0, lines.indexOfFirst { it.isBlank() })
+            .associate { wireValue ->
+                val (wire, value) = wireValue.split(": ")
+                Pair(wire, value.toInt())
+            }
+        wireToValue.putAll(values)
+
+        val edges = lines.subList(lines.indexOfFirst { it.isBlank() } + 1, lines.size)
+            .map { Edge.parse(it) }
+
+        val wirePairToEdges = edges.groupBy { it.wirePair() }
+        return Pair(wireToValue, wirePairToEdges)
+    }
+
+    private fun valueOfWires(wireToValue: MutableMap<String, Int>, prefix: String = "z"): String {
+        return wireToValue.filter { (k, _) -> k.startsWith(prefix) }
             .keys
             .sortedDescending()
             .map { k -> wireToValue[k]!! }
             .joinToString("")
-            .let { it.toLong(2) }
-            .let { println(it) }
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        if (lines.isEmpty()) println("empty lines") else println(lines.size)
+        /** Based on forensic analysis of scuff marks
+         * and scratches on the device,
+         * you can tell that there are exactly four
+         * pairs of gates whose output wires have been swapped
+         *
+         * A gate can only be in at most one such pair
+         *
+         * */
+        val (wireToValue, wirePairToEdges) = parse(lines)
+
+        compute(wireToValue, wirePairToEdges)
+
+        val x = valueOfWires(wireToValue, "x")
+        val y = valueOfWires(wireToValue, "y")
+        val z = valueOfWires(wireToValue, "z")
+
+        println("x =  $x")
+        println("y =  $y")
+        println("z = $z")
+
+        val expectedZ = (x.toLong(2) + y.toLong(2)).toString(2)
+        println("z = $expectedZ (expected)")
     }
 
     data class Edge(val left: String, val right: String, val operation: Operation, val dest: String) {
