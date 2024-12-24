7c32d948c48888f0e0230711cbfd8bc3b9660d81
kasptom <kasptom@gmail.com>
2024-12-24 11:10:11 +0100

    (Day 24) Crossed Wires - wip: part 2 - analyzing

--- src/main/kotlin/year2024/Day24.kt
+++ src/main/kotlin/year2024/Day24.kt
@@ -1,7 +1,13 @@
 package year2024
 
 import aoc.IAocTaskKt
-import utils.except
+import org.jgrapht.Graph
+import org.jgrapht.graph.SimpleGraph
+import org.jgrapht.nio.Attribute
+import org.jgrapht.nio.DefaultAttribute
+import org.jgrapht.nio.dot.DOTExporter
+import java.io.StringWriter
+import java.io.Writer
 
 class Day24 : IAocTaskKt {
     override fun getFileName(): String = "aoc2024/input_24.txt"
@@ -90,53 +96,63 @@ class Day24 : IAocTaskKt {
         println("edges count")
         println(wirePairToEdges.values.flatten().size)
 
-        val edges = wirePairToEdges.values.flatten()
 
         val (initialWireToValue, initialWirePairToEdges) = parse(lines)
-        for (firstEdge in edges) {
-            for (secondEdge in edges.except(firstEdge)) {
-                for (thirdEdge in edges.except(firstEdge, secondEdge)) {
-                    for (fourthEdge in edges.except(firstEdge, secondEdge, thirdEdge)) {
 
-                        val firstWirePair = firstEdge.wirePair()
-                        val secondWirePair = secondEdge.wirePair()
-                        val thirdWirePair = thirdEdge.wirePair()
-                        val fourthWirePair = fourthEdge.wirePair()
-                        val wirePairs = (firstWirePair + secondWirePair + thirdWirePair + fourthWirePair)
-                            .sorted()
-                            .toList()
-                        if (wirePairs.size != 8) {
-                            continue
-                        }
-
-                        val updatedWirePairToEdges = initialWirePairToEdges.toMutableMap()
-                            .mapValues { (_, v) -> v.toMutableSet() }
-
-                        val (swappedFirstEdge, swappedSecondEdge) = firstEdge.swapOutput(secondEdge)
-                        updatedWirePairToEdges[firstWirePair]!!.remove(firstEdge)
-                        updatedWirePairToEdges[firstWirePair]!!.add(swappedFirstEdge)
-                        updatedWirePairToEdges[secondWirePair]!!.remove(secondEdge)
-                        updatedWirePairToEdges[secondWirePair]!!.add(swappedSecondEdge)
-
-                        val (swappedThirdEdge, swappedFourthEdge) = thirdEdge.swapOutput(fourthEdge)
-                        updatedWirePairToEdges[thirdWirePair]!!.remove(thirdEdge)
-                        updatedWirePairToEdges[thirdWirePair]!!.add(swappedThirdEdge)
-                        updatedWirePairToEdges[fourthWirePair]!!.remove(fourthEdge)
-                        updatedWirePairToEdges[fourthWirePair]!!.add(swappedFourthEdge)
-
-                        val swappedWirePairToEdges = updatedWirePairToEdges
-                            .mapValues { (_, v) -> v.toList() }
-                        compute(initialWireToValue.toMutableMap(), swappedWirePairToEdges)
-
-                        val zAfterSwap = valueOfWires(wireToValue, "z")
-                        if (zAfterSwap == expectedZ) {
-                            println(wirePairs.joinToString(","))
-                            return
-                        }
-                    }
-                }
+        // visualization
+        val vertices = (initialWirePairToEdges.values.flatten().map { it.left }
+                        + initialWirePairToEdges.values.flatten().map { it.right }
+                        + initialWirePairToEdges.values.flatten().map { it.dest }).toSet()
+        val edges = initialWirePairToEdges.values.flatten().toSet()
+        // visualisation
+//        viewGraph(vertices, edges)
+        // x =   ...111111
+        // y =  ...000001
+        // expected =
+        // z = 1...0000000
+        val input = initialWireToValue.map { (key, _) ->
+            if (key.startsWith("y")) {
+                Pair(key, 1)
+            } else if (key == "x00") {
+                Pair(key, 1)
+            } else if (key.startsWith("x")) {
+                Pair(key, 0)
+            } else {
+                throw IllegalStateException()
             }
-        }
+        }.toMap()
+            .toMutableMap()
+        compute(input, wirePairToEdges)
+        println(" x=${valueOfWires(input, "x")}")
+        println(" y=${valueOfWires(input, "y")}")
+        println("z=${valueOfWires(input, "z")}")
+        // z06 and z023
+        val edgesWith06End = wirePairToEdges.values.flatten().filter { it.dest == "z06" }
+        val edgesWith23End = wirePairToEdges.values.flatten().filter { it.dest == "z23" }
+
+        assert(edgesWith06End.size == 1)
+        assert(edgesWith23End.size == 1)
+
+        println(edgesWith06End)
+        println(edgesWith23End)
+        var newWirePairToEdges = wirePairToEdges
+            .mapValues { (_, v) -> v.toMutableList() }
+            .toMutableMap()
+
+        val edgeWithZ06 = edgesWith06End[0]
+        val edgeWithZ23 = edgesWith23End[0]
+
+        val (swappedA, swappedB) = edgeWithZ06.swapOutput(edgeWithZ23)
+        newWirePairToEdges[swappedA.wirePair()]!!.remove(edgeWithZ06)
+        newWirePairToEdges[swappedA.wirePair()]!!.add(swappedA)
+
+        newWirePairToEdges[swappedB.wirePair()]!!.remove(edgeWithZ23)
+        newWirePairToEdges[swappedB.wirePair()]!!.add(swappedB)
+
+        compute(input, newWirePairToEdges)
+        println(" x=${valueOfWires(input, "x")}")
+        println(" y=${valueOfWires(input, "y")}")
+        println("z=${valueOfWires(input, "z")}")
     }
 
     data class Edge(val left: String, val right: String, val operation: Operation, val dest: String) {
@@ -157,6 +173,9 @@ class Day24 : IAocTaskKt {
             return Pair(a, b)
         }
 
+        fun operationEdge(): GraphEdge = GraphEdge(left, right, operation.toSymbol())
+        fun rightToDestEdge(): GraphEdge = GraphEdge(right, dest, "=")
+
         enum class Operation {
             XOR, OR, AND;
 
@@ -165,7 +184,15 @@ class Day24 : IAocTaskKt {
                 OR -> leftVal.or(rightVal)
                 AND -> leftVal.and(rightVal)
             }
+
+            fun toSymbol(): String = when (this) {
+                XOR -> "XOR"
+                OR -> "OR"
+                AND -> "AND"
+            }
         }
+
+        data class GraphEdge(val from: String, val to: String, val label: String)
 
         companion object {
             fun parse(leftRightDest: String): Edge {
@@ -175,5 +202,48 @@ class Day24 : IAocTaskKt {
                 return Edge(left, right, operation, dest)
             }
         }
+    }
+
+    fun viewGraph(vertices: Set<String>, edges: Set<Edge>) {
+        val graph: Graph<String, Edge.GraphEdge> = SimpleGraph(Edge.GraphEdge::class.java)
+
+        vertices.sorted().forEach {
+            graph.addVertex(it)
+        }
+        edges.sortedBy { it.dest }.forEach {
+            graph.addEdge(it.left, it.right, it.operationEdge())
+            graph.addEdge(it.right, it.dest, it.rightToDestEdge())
+        }
+        renderGraph(graph)
+    }
+
+    private fun renderGraph(graph: Graph<String, Edge.GraphEdge>) {
+        val exporter: DOTExporter<String, Edge.GraphEdge> =
+            DOTExporter { v -> v }
+        exporter.setEdgeAttributeProvider { e ->
+            val map: MutableMap<String, Attribute> =
+                LinkedHashMap()
+            map["label"] = DefaultAttribute.createAttribute(e.label)
+            map["orientation"] = DefaultAttribute.createAttribute("portrait")
+            map
+        }
+        exporter.setVertexAttributeProvider { v ->
+            val map: MutableMap<String, Attribute> =
+                LinkedHashMap()
+            map["label"] = DefaultAttribute.createAttribute(v.toString())
+            map["orientation"] = DefaultAttribute.createAttribute("portrait")
+            map
+        }
+        exporter.setGraphAttributeProvider {
+            val map: MutableMap<String, Attribute> =
+                LinkedHashMap()
+            map["orientation"] = DefaultAttribute.createAttribute("landscape")
+            map["ordering"] = DefaultAttribute.createAttribute("out")
+            map
+        }
+
+        val writer: Writer = StringWriter()
+        exporter.exportGraph(graph, writer)
+        println(writer)
     }
 }
