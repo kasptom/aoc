92dcb3599e21fabcee4c2ee5715fe03b7c339808
kasptom <kasptom@gmail.com>
2025-12-01 06:56:33 +0100

    (Day 01) Secret entrance - part 2

--- src/main/kotlin/year2025/Day01.kt
+++ src/main/kotlin/year2025/Day01.kt
@@ -3,32 +3,40 @@ package year2025
 import aoc.IAocTaskKt
 
 class Day01 : IAocTaskKt {
-    override fun getFileName(): String =  "aoc2025/input_01.txt"
+    override fun getFileName(): String = "aoc2025/input_01.txt"
 
     override fun solvePartOne(lines: List<String>) {
         var x = 50
         var pass = 0
         for (line in lines) {
             val value = line.substring(1).toInt()
-            if (line.startsWith("L")) {
-                x -= value
-                while (x < 0) {
-                    x += 100
-                }
-                x %= 100
-            } else {
-                x += value
-                x %= 100
-            }
+            val sign = if (line.startsWith("L")) -1 else 1
+            x += sign * value
+            x += 100
+            x %= 100
             if (x == 0) {
                 pass++
             }
-            println("line: $line, value: $value, x: $x, pass: $pass")
+//            println("The dial is rotated $line to point at $x")
         }
-        println("OUTPUT: $pass <--")
+        println(pass)
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        println("part 2 2025")
+        var x = 50
+        var pass = 0
+        for (line in lines) {
+            val value = line.substring(1).toInt()
+            val sign = if (line.startsWith("L")) -1 else 1
+            repeat(value) { _ ->
+                x += sign
+                if (x == 0 || x == 100) {
+                    pass++
+                }
+                x += 100
+                x %= 100
+            }
+        }
+        println(pass)
     }
 }
