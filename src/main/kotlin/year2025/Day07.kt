b0b3b7f6192fd6a56c2ead1e6ea3162cd4e1559e
kasptom <kasptom@gmail.com>
2025-12-07 07:10:21 +0100

    (Day 07) Laboratories - part 2

--- src/main/kotlin/year2025/Day07.kt
+++ src/main/kotlin/year2025/Day07.kt
@@ -25,14 +25,48 @@ class Day07 : IAocTaskKt {
         }
         for (i in 1 until grid.size) {
             for (j in 1 until grid[0].size - 1)
-            if (grid[i][j] == "^" && grid[i - 1][j] == "|") {
-                splits++
-            }
+                if (grid[i][j] == "^" && grid[i - 1][j] == "|") {
+                    splits++
+                }
         }
         println(splits)
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        TODO("Not yet implemented")
+        val grid = lines
+            .map { it.chunked(1).toMutableList() }
+            .toMutableList()
+
+        val start = grid[0].indexOf("S")
+        grid[1][start] = "|"
+        val memo = mutableMapOf<Pair<Int, Int>, Long>()
+        val timelines = countTimelines(grid, 1, start, memo)
+        println(timelines)
+    }
+
+    private fun countTimelines(
+        grid: MutableList<MutableList<String>>,
+        currentRow: Int,
+        currentCol: Int,
+        memo: MutableMap<Pair<Int, Int>, Long>,
+    ): Long {
+        if (currentRow == grid.size - 1) {
+            return 1
+        }
+        if (memo.containsKey(Pair(currentRow, currentCol))) {
+            return memo[Pair(currentRow, currentCol)]!!
+        }
+        var timelines = 0L
+        if (grid[currentRow][currentCol] == "|" && grid[currentRow + 1][currentCol] == "^") {
+            grid[currentRow + 1][currentCol - 1] = "|"
+            timelines += countTimelines(grid, currentRow + 1, currentCol - 1, memo)
+            grid[currentRow][currentCol + 1] = "|"
+            timelines += countTimelines(grid, currentRow + 1, currentCol + 1, memo)
+        } else {
+            grid[currentRow + 1][currentCol] = "|"
+            timelines += countTimelines(grid, currentRow + 1, currentCol, memo)
+        }
+        memo[Pair(currentRow, currentCol)] = timelines
+        return timelines
     }
 }
