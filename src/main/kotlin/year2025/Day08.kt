b1b7e93cd390e3e806d0e077533d6f67e524e72a
kasptom <kasptom@gmail.com>
2025-12-08 07:21:10 +0100

    (Day 08) Playground - part 2

--- src/main/kotlin/year2025/Day08.kt
+++ src/main/kotlin/year2025/Day08.kt
@@ -9,53 +9,13 @@ class Day08 : IAocTaskKt {
 
     override fun solvePartOne(lines: List<String>) {
         val connections = if (getFileName() == "aoc2025/input_08_test.txt") 10 else 1000
-        val points = lines.map { line -> Point.parse(line) }.toSet()
-        points.groupBy { point -> point.equclideanDistance(points.minOf { it }).toInt() }
-        val pointPairToDistance = mutableMapOf<Pair<Point, Point>, Double>()
-        val distanceToPair = TreeMap<Double, MutableList<Pair<Point, Point>>>()
-        for (point in points) {
-            for (other in points) {
-                if (point == other) {
-                    continue
-                }
-                val key = point.combineWith(other)
-                if (pointPairToDistance.containsKey(key).not()) {
-                    val distance = point.equclideanDistance(other)
-                    pointPairToDistance[key] = distance
-                    distanceToPair.putIfAbsent(distance, mutableListOf())
-                    distanceToPair[distance]!!.add(key)
-                }
-            }
-        }
-//        distanceToPair.forEach { (distance, pairs) -> println("$distance -> $pairs") }
-
-        val groups = mutableListOf<MutableSet<Point>>()
+        val (_, distanceToPair, groups) = setup(lines)
         var counter = 0
 
         while (counter < connections) {
             val connection = getNextConnection(distanceToPair)
             counter++
-
-            val existingGroup = groups.firstOrNull { it.contains(connection.first) }
-            val existingSecondGroup = groups.firstOrNull { it.contains(connection.second) }
-
-            if (existingGroup != null && existingSecondGroup != null && existingGroup === existingSecondGroup) {
-                continue
-            }
-
-            if (existingGroup != null && existingSecondGroup != null) {
-                groups.remove(existingSecondGroup)
-                existingGroup.addAll(existingSecondGroup)
-            } else if (existingGroup != null) {
-                existingGroup.add(connection.second)
-            } else if (existingSecondGroup != null) {
-                existingSecondGroup.add(connection.first)
-            } else {
-                val set = mutableSetOf<Point>()
-                set.add(connection.first)
-                set.add(connection.second)
-                groups.add(set)
-            }
+            addConnection(groups, connection)
         }
         val topThree = groups.sortedByDescending { it.size }
             .subList(0, 3)
@@ -74,11 +34,78 @@ class Day08 : IAocTaskKt {
     }
 
     override fun solvePartTwo(lines: List<String>) {
-        TODO("Not yet implemented")
+        val (points, distanceToPair, groups) = setup(lines)
+        var counter = 0
+
+        while (true) {
+            val connection = getNextConnection(distanceToPair)
+            counter++
+
+            if (addConnection(groups, connection)) {
+                continue
+            }
+
+            if (groups.size == 1 && groups.first().size == points.size) {
+                println(connection.first.x.toLong() * connection.second.x.toLong())
+                break
+            }
+        }
+    }
+
+    private fun addConnection(
+        groups: MutableList<MutableSet<Point>>,
+        connection: Pair<Point, Point>,
+    ): Boolean {
+        val existingGroup = groups.firstOrNull { it.contains(connection.first) }
+        val existingSecondGroup = groups.firstOrNull { it.contains(connection.second) }
+
+        if (existingGroup != null && existingSecondGroup != null && existingGroup === existingSecondGroup) {
+            return true
+        }
+
+        if (existingGroup != null && existingSecondGroup != null) {
+            groups.remove(existingSecondGroup)
+            existingGroup.addAll(existingSecondGroup)
+        } else if (existingGroup != null) {
+            existingGroup.add(connection.second)
+        } else if (existingSecondGroup != null) {
+            existingSecondGroup.add(connection.first)
+        } else {
+            val set = mutableSetOf<Point>()
+            set.add(connection.first)
+            set.add(connection.second)
+            groups.add(set)
+        }
+        return false
+    }
+
+    private fun setup(lines: List<String>): Triple<Set<Point>, TreeMap<Double, MutableList<Pair<Point, Point>>>, MutableList<MutableSet<Point>>> {
+        val points = lines.map { line -> Point.parse(line) }.toSet()
+        points.groupBy { point -> point.euclideanDistance(points.minOf { it }).toInt() }
+        val pointPairToDistance = mutableMapOf<Pair<Point, Point>, Double>()
+        val distanceToPair = TreeMap<Double, MutableList<Pair<Point, Point>>>()
+        for (point in points) {
+            for (other in points) {
+                if (point == other) {
+                    continue
+                }
+                val key = point.combineWith(other)
+                if (pointPairToDistance.containsKey(key).not()) {
+                    val distance = point.euclideanDistance(other)
+                    pointPairToDistance[key] = distance
+                    distanceToPair.putIfAbsent(distance, mutableListOf())
+                    distanceToPair[distance]!!.add(key)
+                }
+            }
+        }
+        //        distanceToPair.forEach { (distance, pairs) -> println("$distance -> $pairs") }
+
+        val groups = mutableListOf<MutableSet<Point>>()
+        return Triple(points, distanceToPair, groups)
     }
 
     data class Point(val x: Int, val y: Int, val z: Int) : Comparable<Point> {
-        fun equclideanDistance(other: Point): Double {
+        fun euclideanDistance(other: Point): Double {
             val distX = (x - other.x) * (x - other.x).toDouble()
             val distY = (y - other.y) * (y - other.y).toDouble()
             val distZ = (z - other.z) * (z - other.z).toDouble()
